d1cdce8972de6b7e72668feeab4384ebd375107f
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2012-11-16 18:05:25 -0500

    Filter now uses Rate

--- src/org/mit/jstreamit/Filter.java
+++ src/org/mit/jstreamit/Filter.java
@@ -6,15 +6,45 @@ package org.mit.jstreamit;
  * @since 11/7/2012
  */
 public abstract class Filter<I, O> implements StreamElement<I, O> {
-	//TODO: eventually we'll want to allow ranges, typical values and possibly
-	//sets of rates (for when the work-function has either rate A or rate B
-	//depending on state).
-	private final int popRate, pushRate, peekRate;
+	private final Rate popRate, pushRate, peekRate;
+
+	/**
+	 * Creates a new Filter object with the given pop and push rates and a peek
+	 * rate of 0.
+	 * @param popRate the pop rate
+	 * @param pushRate the push rate
+	 */
+	public Filter(int popRate, int pushRate) {
+		//TODO: should we pass pushRate for peekRate instead of 0?
+		this(popRate, pushRate, 0);
+	}
+
+	/**
+	 * Creates a new Filter object with the given pop, push and peek rates.
+	 * @param popRate the pop rate
+	 * @param pushRate the push rate
+	 * @param peekRate the peek rate
+	 */
 	public Filter(int popRate, int pushRate, int peekRate) {
+		this(Rate.create(popRate), Rate.create(pushRate), Rate.create(peekRate));
+	}
+
+	/**
+	 * Creates a new Filter object with the given pop, push and peek rates.
+	 * @param popRate the pop rate
+	 * @param pushRate the push rate
+	 * @param peekRate the peek rate
+	 */
+	public Filter(Rate popRate, Rate pushRate, Rate peekRate) {
+		if (popRate == null || pushRate == null || peekRate == null)
+			throw new IllegalArgumentException(
+					String.format("pop %s push %s peek %s",
+					popRate, pushRate, peekRate));
 		this.popRate = popRate;
 		this.pushRate = pushRate;
 		this.peekRate = peekRate;
 	}
+
 	public abstract void work();
 
 	//These don't actually do anything, they're just for the JIT to pattern-match against.
