946b332470ebb5bd66c103171487996cfbf0ceda
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-01-23 21:18:27 -0500

    Portal: getHandle() method changes and comments

--- src/org/mit/jstreamit/Portal.java
+++ src/org/mit/jstreamit/Portal.java
@@ -28,14 +28,55 @@ public final class Portal<I> {
 		recipients.add(recipient);
 	}
 
-	public I getHandle(int latency) {
+	/**
+	 * Gets a handle from this portal which can be used to send messages to the
+	 * registered recipients.  This method should only be called from the work()
+	 * function of a filter, splitter or joiner.
+	 *
+	 * The sender argument should always be the this reference of the filter,
+	 * splitter or joiner from which getHandle() is being called; other values
+	 * will result in strange behavior. Unfortunately, the Java language does
+	 * not allow enforcing this requirement.
+	 *
+	 * TODO briefly explain latency
+	 *
+	 * The returned handle appears to be an I instance, but is actually a magic
+	 * object that translates calls of I methods to messages. That is, calling a
+	 * method on the returned handle with some arguments results in that method
+	 * being invoked with those arguments on each of the recipients after the
+	 * specified latency. The argument objects must not be modified until after
+	 * all recipients have received and processed their messages. Handles should
+	 * not be stored in local variables or fields.
+	 *
+	 * Implementation note: this is a JIT hook method.
+	 *
+	 * TODO: PrimitiveWorker is package-private and we're
+	 * leaking it into the public API here.  Alternatives:
+	 * --create three overloads of getHandle: Filter, Splitter and Joiner
+	 * --just make PrimitiveWorker public (probably renamed to Worker and with
+	 *   all the Channel/predecessor/successor stuff still package-private)
+	 *
+	 * TODO: latency ranges?
+	 * @param sender the message sender
+	 * @param latency the message latency
+	 * @return an I whose calls generate messages
+	 */
+	public I getHandle(PrimitiveWorker<?, ?> sender, int latency) {
+		if (sender == null)
+			throw new NullPointerException();
 		//When running in the JIT compiler, this is a JIT hook instead.
-		return (I)Proxy.newProxyInstance(klass.getClassLoader(), new Class[]{klass}, new LatencyInvocationHandler(latency));
+		@SuppressWarnings("unchecked")
+		I handle = (I)Proxy.newProxyInstance(klass.getClassLoader(), new Class<?>[]{klass}, new Handle(sender, recipients, latency));
+		return handle;
 	}
 
-	private class LatencyInvocationHandler implements InvocationHandler {
+	private class Handle implements InvocationHandler {
+		private final PrimitiveWorker<?, ?> sender;
+		private final List<I> recipients;
 		private final int latency;
-		LatencyInvocationHandler(int latency) {
+		private Handle(PrimitiveWorker<?, ?> sender, List<I> recipients, int latency) {
+			this.sender = sender;
+			this.recipients = recipients;
 			this.latency = latency;
 		}
 		@Override
