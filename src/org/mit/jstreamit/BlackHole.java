e24e8fc0afbc5f48ac3853e4680898d6547877c0
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-02-05 04:35:01 -0500

    Sink -> BlackHole to better describe

---
+++ src/org/mit/jstreamit/BlackHole.java
@@ -0,0 +1,20 @@
+package org.mit.jstreamit;
+
+/**
+ * A BlackHole pops data items and discards them. BlackHoles are useful for
+ * terminating stream graphs whose output will not be retrieved, preventing it
+ * from wasting memory in queues.
+ *
+ * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
+ * @since 1/2/2013
+ */
+public class BlackHole<T> extends Filter<T, Void> {
+	public BlackHole() {
+		super(1, 0);
+	}
+
+	@Override
+	public void work() {
+		pop();
+	}
+}
