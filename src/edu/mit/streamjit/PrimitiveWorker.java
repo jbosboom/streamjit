55650964d5832055cbe51e2a0e505f88717efc7d
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-02-14 05:00:10 -0500

    Reorganize PrimitiveWorker to promote public API

--- src/edu/mit/streamjit/PrimitiveWorker.java
+++ src/edu/mit/streamjit/PrimitiveWorker.java
@@ -11,13 +11,50 @@ import java.util.Queue;
 import java.util.Set;
 
 /**
- * A PrimitiveWorker encapsulates a primitive worker in the stream graph (a
- * Filter, Splitter or Joiner) and manages its connections to other workers in
- * support of the interpreter.
+ * A PrimitiveWorker is a StreamElement with a work function, rates, and the
+ * ability to receive teleport messages via a Portal.
+ * <p/>
+ * Ideally, subclasses would provide their rates to a PrimitiveWorker
+ * constructor. But that would require splitters and joiners to commit to their
+ * rates before knowing how many outputs/inputs they have. If the user wants to
+ * build a splitjoin with a variable number of elements, he or she would have to
+ * figure out how many before constructing the splitjoin, rather than being able
+ * to call Splitjoin.add() in a loop. Even if the user wants a fixed number of
+ * elements in the splitjoin, he or she shouldn't have to repeat him or herself
+ * by specifying a splitter size, joiner size and then adding that many elements
+ * (classic StreamIt doesn't require this, for example). Thus, rates are instead
+ * provided by abstract methods. Note that Filter, being
+ * single-input-single-output, does manage its rates for you.
  * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
  * @since 11/19/2012
  */
 public abstract class PrimitiveWorker<I, O> implements StreamElement<I, O> {
+	public abstract void work();
+
+	/**
+	 * Returns a list of peek rates, such that the rate for the i-th channel is
+	 * returned at index i.  Workers with only one input will return a singleton
+	 * list.
+	 * @return a list of peek rates
+	 */
+	abstract List<Rate> getPeekRates();
+
+	/**
+	 * Returns a list of pop rates, such that the rate for the i-th channel is
+	 * returned at index i.  Workers with only one input will return a singleton
+	 * list.
+	 * @return a list of pop rates
+	 */
+	abstract List<Rate> getPopRates();
+
+	/**
+	 * Returns a list of push rates, such that the rate for the i-th channel is
+	 * returned at index i. Workers with only one output will return a singleton
+	 * list.
+	 * @return a list of push rates
+	 */
+	abstract List<Rate> getPushRates();
+
 	private List<PrimitiveWorker<?, ? extends I>> predecessors = new ArrayList<>(1);
 	private List<PrimitiveWorker<? super O, ?>> successors = new ArrayList<>(1);
 	private List<Channel<? extends I>> inputChannels = new ArrayList<>(1);
@@ -58,8 +95,6 @@ public abstract class PrimitiveWorker<I, O> implements StreamElement<I, O> {
 		return outputChannels;
 	}
 
-	public abstract void work();
-
 	/**
 	 * Called by interpreters to do work and other related tasks, such as
 	 * processing messages.
@@ -97,43 +132,6 @@ public abstract class PrimitiveWorker<I, O> implements StreamElement<I, O> {
 			insertionPoint = -(insertionPoint + 1);
 		messages.add(insertionPoint, message);
 	}
-
-	/*
-	 * Ideally, subclasses would provide their rates to our constructor.  But
-	 * that would require splitters and joiners to commit to their rates before
-	 * knowing how many outputs/inputs they have.  If the user wants to build
-	 * a splitjoin with a variable number of elements, he or she would have to
-	 * figure out how many before constructing the splitjoin, rather than being
-	 * able to call Splitjoin.add() in a loop.  Even if the user wants a fixed
-	 * number of elements in the splitjoin, he or she shouldn't have to repeat
-	 * him or herself by specifying a splitter size, joiner size and then adding
-	 * that many elements (the StreamIt language doesn't require this, for
-	 * example).
-	 */
-
-	/**
-	 * Returns a list of peek rates, such that the rate for the i-th channel is
-	 * returned at index i.  Workers with only one input will return a singleton
-	 * list.
-	 * @return a list of peek rates
-	 */
-	abstract List<Rate> getPeekRates();
-
-	/**
-	 * Returns a list of pop rates, such that the rate for the i-th channel is
-	 * returned at index i.  Workers with only one input will return a singleton
-	 * list.
-	 * @return a list of pop rates
-	 */
-	abstract List<Rate> getPopRates();
-
-	/**
-	 * Returns a list of push rates, such that the rate for the i-th channel is
-	 * returned at index i. Workers with only one output will return a singleton
-	 * list.
-	 * @return a list of push rates
-	 */
-	abstract List<Rate> getPushRates();
 
 	//<editor-fold defaultstate="collapsed" desc="Friend pattern support (see impl.common.Workers)">
 	private static class WorkersFriend extends edu.mit.streamjit.impl.common.Workers {
