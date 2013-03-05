29c6ec1122865530c9e593847e28f5580fb92077
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-03-05 09:05:09 -0500

    Move RoundrobinJoiner to API package

--- src/edu/mit/streamjit/apps/fmradio/FMRadio.java
+++ src/edu/mit/streamjit/apps/fmradio/FMRadio.java
@@ -2,7 +2,7 @@ package edu.mit.streamjit.apps.fmradio;
 
 import edu.mit.streamjit.api.Splitjoin;
 import edu.mit.streamjit.api.Pipeline;
-import edu.mit.streamjit.RoundrobinJoiner;
+import edu.mit.streamjit.api.RoundrobinJoiner;
 import edu.mit.streamjit.api.CompiledStream;
 import edu.mit.streamjit.api.StreamCompiler;
 import edu.mit.streamjit.api.Filter;
