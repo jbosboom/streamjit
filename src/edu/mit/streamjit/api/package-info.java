72a5b3613c64a5fb9cba0551a1d4374f69850440
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-03-05 13:01:50 -0500

    package-info for API package

---
+++ src/edu/mit/streamjit/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the interface to StreamJIT, as well as some useful predefined
+ * workers.
+ *
+ * TODO: split into interface and util packages?  Maybe just once more
+ * predefined workers are created.
+ */
+package edu.mit.streamjit.api;
