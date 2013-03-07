07a092ea49ace8ebc37e3ac06a9d0e53864a6d85
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-03-07 11:47:20 -0500

    RoundrobinJoiner takes itemsPerExecution argument

--- src/edu/mit/streamjit/api/RoundrobinJoiner.java
+++ src/edu/mit/streamjit/api/RoundrobinJoiner.java
@@ -4,18 +4,27 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- *
+ * A RoundrobinJoiner joins its input by reading data items from each child in
+ * turn per execution.  RoundrobinJoiner reads the same number of items per
+ * child; to use weights, see WeightedRoundrobinJoiner.
  * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
  * @since 11/7/2012
  */
 public class RoundrobinJoiner<T> extends Joiner<T, T> {
+	private final int itemsPerExecution;
 	public RoundrobinJoiner() {
+		this(1);
+	}
+
+	public RoundrobinJoiner(int itemsPerExecution) {
+		this.itemsPerExecution = itemsPerExecution;
 	}
 
 	@Override
 	public void work() {
 		for (int i = 0; i < inputs(); ++i)
-			push(pop(i));
+			for (int j = 0; j < itemsPerExecution; ++j)
+				push(pop(i));
 	}
 
 	@Override
@@ -31,11 +40,11 @@ public class RoundrobinJoiner<T> extends Joiner<T, T> {
 
 	@Override
 	public List<Rate> getPopRates() {
-		return Collections.nCopies(inputs(), Rate.create(1));
+		return Collections.nCopies(inputs(), Rate.create(itemsPerExecution));
 	}
 
 	@Override
 	public List<Rate> getPushRates() {
-		return Collections.singletonList(Rate.create(inputs()));
+		return Collections.singletonList(Rate.create(itemsPerExecution*inputs()));
 	}
 }
