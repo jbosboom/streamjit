c86310fa86bc9e92eaf2574f969cb1b9ae0192bb
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-03-22 17:45:45 -0400

    ChannelFactory comment changes

--- src/edu/mit/streamjit/impl/interp/ChannelFactory.java
+++ src/edu/mit/streamjit/impl/interp/ChannelFactory.java
@@ -9,19 +9,21 @@ import edu.mit.streamjit.api.Worker;
  */
 public interface ChannelFactory {
 	/**
-	 * Creates a channel that will be used to connect the two given workers.
-	 * Implementations should never return null from this method; if an
-	 * implementation doesn't care what channel is used to connect the two
-	 * filters, consider returning EmptyChannel.  Implementations need not
-	 * create a new channel for each call, though not doing so may produce
-	 * strange results.
-	 *
+	 * Creates a channel that will be used to connect the two given workers. If
+	 * an implementation doesn't care what channel is used to connect the two
+	 * filters, consider returning EmptyChannel. Implementations need not create
+	 * a new channel for each call, though not doing so may produce strange
+	 * results. Implementations may return null; the results may vary from not
+	 * connecting the two workers with a channel to throwing an exception.
+	 * <p/>
 	 * TODO: Generic bounds are too strict -- the filters don't have to exactly
-	 * agree on type, but merely be compatible.  But note that isn't of much
+	 * agree on type, but merely be compatible. But note that isn't of much
 	 * import due to erasure.
 	 * @param <E> the type of element in the channel
-	 * @param upstream the upstream worker (adds elements to the channel)
-	 * @param downstream the downstream worker (removes elements from the channel)
+	 * @param upstream the upstream worker (adds elements to the channel), or
+	 * null for the overall stream graph input
+	 * @param downstream the downstream worker (removes elements from the
+	 * channel), or null for the overall stream graph output
 	 * @return a channel to connect the two workers
 	 */
 	public <E> Channel<E> makeChannel(Worker<?, E> upstream, Worker<E, ?> downstream);
