318595a643f278af9639ff0ac73cf6372223a645
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-03-26 18:58:17 -0400

    JSON package info explaining how to add a type

--- src/edu/mit/streamjit/util/json/package-info.java
+++ src/edu/mit/streamjit/util/json/package-info.java
@@ -1,7 +1,19 @@
 /**
- * Contains interfaces for a simple JSON object binding system.  Classes wishing
- * to participate in the binding should register corresponding Jsonifiers and
- * JsonifierFactories using the ServiceLoader mechanism.
+ * Contains interfaces for a simple JSON object binding system.
+ *
+ * To add support for a type:
+ * <ol>
+ * <li>Implement a Jsonifier that can convert instances of the type to and
+ * from JSON values.</li>
+ * <li>Implement a JsonifierFactory that responds to Class objects representing
+ * the type by returning the Jsonifier from the previous step.</li>
+ * <li>Register the JsonifierFactory in
+ * src/META-INF/services/edu.mit.streamjit.util.json.JsonifierFactory.</li>
+ * </ol>
+ *
+ * Note that one JsonifierFactory can return Jsonifier instances for multiple
+ * types and Jsonifier can implement conversion for multiple types.  Also note
+ * that Jsonifier and JsonifierFactory can be the same class.
  *
  * This system does not serialize reference graphs; if an object is encountered
  * multiple times, it will be serialized multiple times and deserialized into
