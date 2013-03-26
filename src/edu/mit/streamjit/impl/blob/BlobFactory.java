f9a24094ce417b5ddf58d503c37b080066329530
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-03-26 18:20:37 -0400

    BlobFactory interface

---
+++ src/edu/mit/streamjit/impl/blob/BlobFactory.java
@@ -0,0 +1,41 @@
+package edu.mit.streamjit.impl.blob;
+
+import edu.mit.streamjit.api.Worker;
+import edu.mit.streamjit.impl.common.Configuration;
+import java.util.Set;
+
+/**
+ * A BlobFactory creates Blobs.
+ *
+ * Instances of this class should be stateless and immutable; they must at least
+ * be thread-safe and reentrant.
+ * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
+ * @since 3/26/2013
+ */
+public interface BlobFactory {
+	/**
+	 * Creates a new Blob responsible for the given set of workers using the
+	 * given configuration that will run on at most the given number of cores.
+	 *
+	 * Not all factories can make blobs for arbitrary sets of workers, so
+	 * callers should be prepared to handle IllegalArgumentException.
+	 *
+	 * Some
+	 * factories may require particular information in their configuration and
+	 * may throw if it is not present or is of a form they do not understand
+	 * (such as a parameter being the wrong type). However, factories must not throw just
+	 * because they were given configuration parameters they do not know about;
+	 * instead, they must ignore the extra information.
+	 *
+	 * Factories need not produce blobs that utilize all of their allotted
+	 * cores; callers should use the getCoreCount() method of the returned Blob
+	 * to decide how many threads to start, etc.
+	 * @param workers the set of workers to be placed in the blob (must be
+	 * nonempty)
+	 * @param config the configuration used to make a blob
+	 * @param maxNumCores the maximum number of cores the blob may use (must be
+	 * >= 1)
+	 * @return a Blob responsible for the given workers
+	 */
+	public Blob makeBlob(Set<Worker<?, ?>> workers, Configuration config, int maxNumCores);
+}
