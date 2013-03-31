12bb58b08ed9576c2946d701dbc06c00fc186023
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-03-30 21:56:05 -0400

    MethodType.fromDescriptor

--- src/edu/mit/streamjit/impl/compiler/MethodType.java
+++ src/edu/mit/streamjit/impl/compiler/MethodType.java
@@ -21,6 +21,25 @@ public class MethodType extends Type {
 	public static MethodType of(RegularType returnType, RegularType... argumentTypes) {
 		return new MethodType(returnType, argumentTypes);
 	}
+	/**
+	 * Creates a MethodType from a JVM method descriptor.  If the method is an
+	 * instance method, the given receiverType is prepended to the argument list
+	 * from the descriptor.  (The descriptor does not contain implicit this
+	 * parameters; see the JVMS 4.3.3.)
+	 * @param descriptor a JVM method descriptor
+	 * @param receiverType the receiver type, or null if the method is static
+	 * @return a MethodType
+	 */
+	public static MethodType fromDescriptor(String descriptor, RegularType receiverType) {
+		java.lang.invoke.MethodType mt = java.lang.invoke.MethodType.fromMethodDescriptorString(descriptor, null);
+		RegularType returnType = RegularType.of(mt.returnType());
+		RegularType[] argumentTypes = new RegularType[mt.parameterCount() + (receiverType != null ? 1 : 0)];
+		int i = 0;
+		if (receiverType != null)
+			argumentTypes[++i] = receiverType;
+		System.arraycopy(mt.parameterArray(), 0, argumentTypes, i, mt.parameterCount());
+		return MethodType.of(returnType, argumentTypes);
+	}
 	public RegularType getReturnType() {
 		return returnType;
 	}
