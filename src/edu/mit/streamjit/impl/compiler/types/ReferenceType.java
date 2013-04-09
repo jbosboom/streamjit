f9596a2e5addf1ab90a35f72878c83733a043d3f
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-04-09 15:05:25 -0400

    ReferenceType includes interfaces
    At the Java bytecode level, all interfaces have Object as their
    direct superclass (only java.lang.Object has no superclass).  But
    Java reflection doesn't expose this, so we need to check for the
    interface modifier bit.

--- src/edu/mit/streamjit/impl/compiler/types/ReferenceType.java
+++ src/edu/mit/streamjit/impl/compiler/types/ReferenceType.java
@@ -2,6 +2,7 @@ package edu.mit.streamjit.impl.compiler.types;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import edu.mit.streamjit.impl.compiler.Klass;
+import edu.mit.streamjit.impl.compiler.Modifier;
 
 /**
  * A reference type, including array types.
@@ -13,8 +14,11 @@ public class ReferenceType extends RegularType {
 		super(klass);
 		//java.lang.Object, or
 		checkArgument(Object.class.equals(klass.getBackingClass()) ||
-				//something with a superclass (includes array types).
-				klass.getSuperclass() != null,
+				//something with a superclass (includes array types), or
+				klass.getSuperclass() != null ||
+				//an interface (which subclasses Object at the bytecode level,
+				//but not according to Java reflection).
+				klass.modifiers().contains(Modifier.INTERFACE),
 				"not a ReferenceType: %s", klass);
 	}
 }
--- src/edu/mit/streamjit/impl/compiler/types/RegularType.java
+++ src/edu/mit/streamjit/impl/compiler/types/RegularType.java
@@ -2,6 +2,7 @@ package edu.mit.streamjit.impl.compiler.types;
 
 import static com.google.common.base.Preconditions.*;
 import edu.mit.streamjit.impl.compiler.Klass;
+import edu.mit.streamjit.impl.compiler.Modifier;
 
 /**
  * A RegularType is a primitive or reference type.
@@ -16,8 +17,11 @@ public abstract class RegularType extends ReturnType {
 		checkArgument((backing != null && backing.isPrimitive() && !backing.equals(void.class)) ||
 				//java.lang.Object, or
 				(backing != null && backing.equals(Object.class)) ||
-				//something with a superclass (includes array types).
-				klass.getSuperclass() != null,
+				//something with a superclass (includes array types), or
+				klass.getSuperclass() != null ||
+				//an interface (which subclasses Object at the bytecode level,
+				//but not according to Java reflection).
+				klass.modifiers().contains(Modifier.INTERFACE),
 				"not a RegularType: %s", klass);
 	}
 }
