19b9f9cc0a55275f374a9d2a0cb8dec856e02a12
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-04-09 15:32:13 -0400

    ReturnType.getModule(), getTypeFactory()

--- src/edu/mit/streamjit/impl/compiler/types/ReturnType.java
+++ src/edu/mit/streamjit/impl/compiler/types/ReturnType.java
@@ -2,6 +2,7 @@ package edu.mit.streamjit.impl.compiler.types;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import edu.mit.streamjit.impl.compiler.Klass;
+import edu.mit.streamjit.impl.compiler.Module;
 
 /**
  * A ReturnType is a type that can be used as the return type of a method; that
@@ -17,6 +18,24 @@ public abstract class ReturnType extends Type {
 
 	public Klass getKlass() {
 		return klass;
+	}
+
+	/**
+	 * Returns this type's module.  All ReturnTypes implicitly belong to a Module (the Module their Klass
+	 * belongs to).
+	 * @return the Module this ReturnType belongs to
+	 */
+	protected Module getModule() {
+		return getKlass().getParent();
+	}
+
+	/**
+	 * Returns this type's TypeFactory.  All ReturnTypes implicitly have a
+	 * TypeFactory (the TypeFactory of the Module they belong to)
+	 * @return the TypeFactory of the Module this ReturnType belongs to
+	 */
+	protected TypeFactory getTypeFactory() {
+		return getModule().types();
 	}
 
 	@Override
