3a648cc55644b7603b7b261401c4f2e62150645b
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-04-11 12:31:46 -0400

    Instruction is a User, not just a Value

--- src/edu/mit/streamjit/impl/compiler/insts/Instruction.java
+++ src/edu/mit/streamjit/impl/compiler/insts/Instruction.java
@@ -2,7 +2,7 @@ package edu.mit.streamjit.impl.compiler.insts;
 
 import edu.mit.streamjit.impl.compiler.BasicBlock;
 import edu.mit.streamjit.impl.compiler.ParentedList;
-import edu.mit.streamjit.impl.compiler.Value;
+import edu.mit.streamjit.impl.compiler.User;
 import edu.mit.streamjit.impl.compiler.types.Type;
 import edu.mit.streamjit.util.IntrusiveList;
 
@@ -11,7 +11,7 @@ import edu.mit.streamjit.util.IntrusiveList;
  * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
  * @since 4/11/2013
  */
-public abstract class Instruction extends Value implements ParentedList.Parented<BasicBlock> {
+public abstract class Instruction extends User implements ParentedList.Parented<BasicBlock> {
 	@IntrusiveList.Previous
 	private Instruction previous;
 	@IntrusiveList.Next
@@ -22,8 +22,17 @@ public abstract class Instruction extends Value implements ParentedList.Parented
 	protected Instruction(Type type) {
 		super(type);
 	}
+
+	protected Instruction(Type type, int operands) {
+		super(type, operands);
+	}
+
 	protected Instruction(Type type, String name) {
 		super(type, name);
+	}
+
+	protected Instruction(Type type, int operands, String name) {
+		super(type, operands, name);
 	}
 
 	@Override
