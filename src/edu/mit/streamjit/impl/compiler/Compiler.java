e66ba63bd60b4afb832f5b11562de3dbb356a6ed
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-04-25 17:52:04 -0400

    Compiler: replaces JIT-hook calls

--- src/edu/mit/streamjit/impl/compiler/Compiler.java
+++ src/edu/mit/streamjit/impl/compiler/Compiler.java
@@ -1,13 +1,19 @@
 package edu.mit.streamjit.impl.compiler;
 
 import static com.google.common.base.Preconditions.*;
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import edu.mit.streamjit.api.Filter;
 import edu.mit.streamjit.api.Identity;
+import edu.mit.streamjit.api.Joiner;
 import edu.mit.streamjit.api.OneToOneElement;
 import edu.mit.streamjit.api.Rate;
 import edu.mit.streamjit.api.RoundrobinJoiner;
 import edu.mit.streamjit.api.RoundrobinSplitter;
 import edu.mit.streamjit.api.Splitjoin;
+import edu.mit.streamjit.api.Splitter;
 import edu.mit.streamjit.api.StatefulFilter;
 import edu.mit.streamjit.api.Worker;
 import edu.mit.streamjit.impl.blob.Blob;
@@ -16,8 +22,21 @@ import edu.mit.streamjit.impl.common.ConnectWorkersVisitor;
 import edu.mit.streamjit.impl.common.IOInfo;
 import edu.mit.streamjit.impl.common.MessageConstraint;
 import edu.mit.streamjit.impl.common.Workers;
+import edu.mit.streamjit.impl.compiler.insts.ArrayLoadInst;
+import edu.mit.streamjit.impl.compiler.insts.ArrayStoreInst;
+import edu.mit.streamjit.impl.compiler.insts.BinaryInst;
 import edu.mit.streamjit.impl.compiler.insts.CallInst;
+import edu.mit.streamjit.impl.compiler.insts.Instruction;
+import edu.mit.streamjit.impl.compiler.insts.JumpInst;
+import edu.mit.streamjit.impl.compiler.insts.LoadInst;
+import edu.mit.streamjit.impl.compiler.insts.NewArrayInst;
 import edu.mit.streamjit.impl.compiler.insts.ReturnInst;
+import edu.mit.streamjit.impl.compiler.insts.StoreInst;
+import edu.mit.streamjit.impl.compiler.types.MethodType;
+import edu.mit.streamjit.impl.compiler.types.RegularType;
+import edu.mit.streamjit.impl.interp.Channel;
+import edu.mit.streamjit.impl.interp.ChannelFactory;
+import edu.mit.streamjit.impl.interp.EmptyChannel;
 import java.io.PrintWriter;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -104,6 +123,7 @@ public final class Compiler {
 		for (Worker<?, ?> w : workers)
 			buildWorkerData(w);
 		addBlobPlumbing();
+		blobKlass.dump(new PrintWriter(System.out, true));
 		return instantiateBlob();
 	}
 
@@ -138,6 +158,233 @@ public final class Compiler {
 				throw new AssertionError("Can't happen!", ex);
 			}
 		}
+
+		makeWorkMethod(worker);
+	}
+
+	/**
+	 * Make the work method for the given worker.  We actually make two methods
+	 * here: first we make a copy with a dummy receiver argument, just to have a
+	 * copy to work with.  After remapping every use of that receiver (remapping
+	 * field accesses to the worker's static fields, remapping JIT-hooks to
+	 * their implementations, and remapping utility methods in the worker class
+	 * recursively), we then create the actual work method without the receiver
+	 * argument.
+	 * @param worker
+	 */
+	private void makeWorkMethod(Worker<?, ?> worker) {
+		WorkerData data = workerData.get(worker);
+		int id = Workers.getIdentifier(worker);
+		int numInputs = getNumInputs(worker);
+		int numOutputs = getNumOutputs(worker);
+		Klass workerKlass = module.getKlass(worker.getClass());
+		Method oldWork = workerKlass.getMethod("work", module.types().getMethodType(void.class, worker.getClass()));
+		oldWork.resolve();
+
+		MethodType workMethodType = makeWorkMethodType(worker);
+		MethodType rworkMethodType = workMethodType.prependArgument(module.types().getRegularType(workerKlass));
+		Method newWork = new Method("rwork"+id, rworkMethodType, EnumSet.of(Modifier.PRIVATE, Modifier.STATIC), blobKlass);
+		newWork.arguments().get(0).setName("dummyReceiver");
+
+		Map<Value, Value> vmap = new IdentityHashMap<>();
+		vmap.put(oldWork.arguments().get(0), newWork.arguments().get(0));
+		Cloning.cloneMethod(oldWork, newWork, vmap);
+
+		data.popCount = new Field(module.types().getRegularType(numInputs > 1 ? int[].class : int.class),
+				"w"+id+"$popCount",
+				EnumSet.of(Modifier.PRIVATE, Modifier.STATIC, Modifier.SYNTHETIC),
+				blobKlass);
+		data.pushCount = new Field(module.types().getRegularType(numOutputs > 1 ? int[].class : int.class),
+				"w"+id+"$pushCount",
+				EnumSet.of(Modifier.PRIVATE, Modifier.STATIC, Modifier.SYNTHETIC),
+				blobKlass);
+
+		BasicBlock entryBlock = new BasicBlock(module, "entry");
+		newWork.basicBlocks().add(0, entryBlock);
+
+		Value popCountInitValue;
+		if (numInputs > 1) {
+			NewArrayInst newArrayInst = new NewArrayInst(module.types().getArrayType(int[].class), module.constants().getConstant(numInputs));
+			newArrayInst.setName("popCountArray");
+			entryBlock.instructions().add(newArrayInst);
+			popCountInitValue = newArrayInst;
+		} else
+			popCountInitValue = module.constants().getConstant(0);
+		StoreInst popCountInit = new StoreInst(data.popCount, popCountInitValue);
+		popCountInit.setName("popCountInit");
+		entryBlock.instructions().add(popCountInit);
+
+		Value pushCountInitValue;
+		if (numOutputs > 1) {
+			NewArrayInst newArrayInst = new NewArrayInst(module.types().getArrayType(int[].class), module.constants().getConstant(numOutputs));
+			newArrayInst.setName("pushCountArray");
+			entryBlock.instructions().add(newArrayInst);
+			pushCountInitValue = newArrayInst;
+		} else
+			pushCountInitValue = module.constants().getConstant(0);
+		StoreInst pushCountInit = new StoreInst(data.pushCount, pushCountInitValue);
+		pushCountInit.setName("pushCountInit");
+		entryBlock.instructions().add(pushCountInit);
+
+		entryBlock.instructions().add(new JumpInst(newWork.basicBlocks().get(1)));
+
+		//Remap stuff in rwork.
+		for (BasicBlock b : newWork.basicBlocks())
+			for (Instruction i : ImmutableList.copyOf(b.instructions()))
+				if (Iterables.contains(i.operands(), newWork.arguments().get(0)))
+					remapEliminiatingReceiver(i, data);
+
+		//TODO: run dead code elimination and verify the receiver arg is gone
+	}
+
+	private MethodType makeWorkMethodType(Worker<?, ?> worker) {
+		RegularType inputParam = module.types().getArrayType(
+				Object.class,
+				worker instanceof Joiner ? 2 : 1);
+		RegularType outputParam;
+		if (worker == lastWorker)
+			outputParam = worker instanceof Splitter ?
+					module.types().getArrayType(Channel.class, 1) :
+					module.types().getRegularType(Channel.class);
+		else
+			outputParam = module.types().getArrayType(Object.class,	worker instanceof Splitter ? 2 : 1);
+		return module.types().getMethodType(module.types().getVoidType(), inputParam, outputParam);
+	}
+
+	private void remapEliminiatingReceiver(Instruction inst, WorkerData data) {
+		BasicBlock block = inst.getParent();
+		Method rwork = inst.getParent().getParent();
+		if (inst instanceof CallInst) {
+			CallInst ci = (CallInst)inst;
+			Method method = ci.getMethod();
+			Klass filterKlass = module.getKlass(Filter.class);
+			Klass splitterKlass = module.getKlass(Splitter.class);
+			Klass joinerKlass = module.getKlass(Joiner.class);
+			Method pop1Filter = filterKlass.getMethod("pop", module.types().getMethodType(Object.class, Filter.class));
+			assert pop1Filter != null;
+			Method pop1Splitter = splitterKlass.getMethod("pop", module.types().getMethodType(Object.class, Splitter.class));
+			assert pop1Splitter != null;
+			Method push1Filter = filterKlass.getMethod("push", module.types().getMethodType(void.class, Filter.class, Object.class));
+			assert push1Filter != null;
+			Method push1Joiner = joinerKlass.getMethod("push", module.types().getMethodType(void.class, Joiner.class, Object.class));
+			assert push1Joiner != null;
+			Method pop2 = joinerKlass.getMethod("pop", module.types().getMethodType(Object.class, Joiner.class, int.class));
+			assert pop2 != null;
+			Method push2 = splitterKlass.getMethod("push", module.types().getMethodType(void.class, Splitter.class, int.class, Object.class));
+			assert push2 != null;
+			Method inputs = joinerKlass.getMethod("inputs", module.types().getMethodType(int.class, Joiner.class));
+			assert inputs != null;
+			Method outputs = splitterKlass.getMethod("outputs", module.types().getMethodType(int.class, Splitter.class));
+			assert outputs != null;
+
+			Method channelPush = module.getKlass(Channel.class).getMethod("push", module.types().getMethodType(void.class, Channel.class, Object.class));
+			assert channelPush != null;
+
+			if (method.equals(pop1Filter) || method.equals(pop1Splitter)) {
+				LoadInst popCount = new LoadInst(data.popCount);
+				ArrayLoadInst item = new ArrayLoadInst(rwork.arguments().get(1), popCount);
+				item.setName("poppedItem");
+				BinaryInst popCountPlusOne = new BinaryInst(popCount, BinaryInst.Operation.ADD, module.constants().getSmallestIntConstant(1));
+				StoreInst updatePopCount = new StoreInst(data.popCount, popCountPlusOne);
+
+				List<Instruction> subList = block.instructions().subList(0, block.instructions().indexOf(inst));
+				subList.add(popCount);
+				subList.add(item);
+				subList.add(popCountPlusOne);
+				subList.add(updatePopCount);
+
+				inst.replaceAllUsesWith(item);
+				block.instructions().remove(inst);
+			} else if ((method.equals(push1Filter) || method.equals(push1Joiner)) && data.worker != lastWorker) {
+				Value item = ci.getArgument(1);
+				LoadInst pushCount = new LoadInst(data.pushCount);
+				ArrayStoreInst store = new ArrayStoreInst(rwork.arguments().get(2), pushCount, item);
+				BinaryInst pushCountPlusOne = new BinaryInst(pushCount, BinaryInst.Operation.ADD, module.constants().getSmallestIntConstant(1));
+				StoreInst updatePushCount = new StoreInst(data.popCount, pushCountPlusOne);
+
+				List<Instruction> subList = block.instructions().subList(0, block.instructions().indexOf(inst));
+				subList.add(pushCount);
+				subList.add(store);
+				subList.add(pushCountPlusOne);
+				subList.add(updatePushCount);
+
+				inst.replaceAllUsesWith(item);
+				block.instructions().remove(inst);
+			} else if ((method.equals(push1Filter) || method.equals(push1Joiner)) && data.worker == lastWorker) {
+				Value item = ci.getArgument(1);
+				CallInst pushOntoChannel = new CallInst(channelPush, rwork.arguments().get(2), item);
+
+				List<Instruction> subList = block.instructions().subList(0, block.instructions().indexOf(inst));
+				subList.add(pushOntoChannel);
+
+				inst.replaceAllUsesWith(item);
+				block.instructions().remove(inst);
+			} else if (method.equals(pop2)) {
+				LoadInst popCountArray = new LoadInst(data.popCount);
+				ArrayLoadInst popCount = new ArrayLoadInst(popCountArray, ci.getArgument(1));
+				ArrayLoadInst inputSelect = new ArrayLoadInst(rwork.arguments().get(1), ci.getArgument(1));
+				ArrayLoadInst item = new ArrayLoadInst(inputSelect, popCount);
+				item.setName("poppedItem");
+				BinaryInst popCountPlusOne = new BinaryInst(popCount, BinaryInst.Operation.ADD, module.constants().getSmallestIntConstant(1));
+				ArrayStoreInst updatePopCount = new ArrayStoreInst(popCountArray, ci.getArgument(1), popCountPlusOne);
+
+				List<Instruction> subList = block.instructions().subList(0, block.instructions().indexOf(inst));
+				subList.add(popCountArray);
+				subList.add(popCount);
+				subList.add(inputSelect);
+				subList.add(item);
+				subList.add(popCountPlusOne);
+				subList.add(updatePopCount);
+
+				inst.replaceAllUsesWith(item);
+				block.instructions().remove(inst);
+			} else if (method.equals(push2) && data.worker != lastWorker) {
+				Value item = ci.getArgument(2);
+				LoadInst pushCountArray = new LoadInst(data.pushCount);
+				ArrayLoadInst pushCount = new ArrayLoadInst(pushCountArray, ci.getArgument(1));
+				ArrayLoadInst outputSelect = new ArrayLoadInst(rwork.arguments().get(2), ci.getArgument(1));
+				ArrayStoreInst store = new ArrayStoreInst(outputSelect, pushCount, item);
+				BinaryInst pushCountPlusOne = new BinaryInst(pushCount, BinaryInst.Operation.ADD, module.constants().getSmallestIntConstant(1));
+				ArrayStoreInst updatePushCount = new ArrayStoreInst(pushCountArray, ci.getArgument(1), pushCountPlusOne);
+
+				List<Instruction> subList = block.instructions().subList(0, block.instructions().indexOf(inst));
+				subList.add(pushCountArray);
+				subList.add(pushCount);
+				subList.add(outputSelect);
+				subList.add(store);
+				subList.add(pushCountPlusOne);
+				subList.add(updatePushCount);
+
+				inst.replaceAllUsesWith(item);
+				block.instructions().remove(inst);
+			} else if (method.equals(push2) && data.worker == lastWorker) {
+				Value item = ci.getArgument(2);
+				ArrayLoadInst outputSelect = new ArrayLoadInst(rwork.arguments().get(2), ci.getArgument(1));
+				CallInst pushOntoChannel = new CallInst(channelPush, outputSelect, item);
+
+				List<Instruction> subList = block.instructions().subList(0, block.instructions().indexOf(inst));
+				subList.add(outputSelect);
+				subList.add(pushOntoChannel);
+
+				inst.replaceAllUsesWith(item);
+				block.instructions().remove(inst);
+			} else if (method.equals(outputs)) {
+				inst.replaceAllUsesWith(module.constants().getSmallestIntConstant(getNumOutputs(data.worker)));
+				block.instructions().remove(inst);
+			} else if (method.equals(inputs)) {
+				inst.replaceAllUsesWith(module.constants().getSmallestIntConstant(getNumInputs(data.worker)));
+				block.instructions().remove(inst);
+			} else
+				throw new AssertionError(inst);
+		}
+	}
+
+	private int getNumInputs(Worker<?, ?> w) {
+		return Workers.getInputChannels(w).size();
+	}
+
+	private int getNumOutputs(Worker<?, ?> w) {
+		return Workers.getOutputChannels(w).size();
 	}
 
 	/**
@@ -190,6 +437,7 @@ public final class Compiler {
 		 * inlining or blob initialization purposes.
 		 */
 		private final Map<Field, Object> fieldValues = new IdentityHashMap<>();
+		private Field popCount, pushCount;
 		private WorkerData(Worker<?, ?> worker) {
 			this.worker = worker;
 		}
@@ -197,14 +445,18 @@ public final class Compiler {
 
 	public static void main(String[] args) {
 		OneToOneElement<Integer, Integer> graph = new Splitjoin<>(new RoundrobinSplitter<Integer>(), new RoundrobinJoiner<Integer>(), new Identity<Integer>(), new Identity<Integer>());
-		ConnectWorkersVisitor cwv = new ConnectWorkersVisitor();
+		ConnectWorkersVisitor cwv = new ConnectWorkersVisitor(new ChannelFactory() {
+			@Override
+			public <E> Channel<E> makeChannel(Worker<?, E> upstream, Worker<E, ?> downstream) {
+				return new EmptyChannel<>();
+			}
+		});
 		graph.visit(cwv);
 		Set<Worker<?, ?>> workers = Workers.getAllWorkersInGraph(cwv.getSource());
 		Configuration config = Configuration.builder().build();
 		int maxNumCores = 1;
 		Compiler compiler = new Compiler(workers, config, maxNumCores);
 		Blob blob = compiler.compile();
-		compiler.blobKlass.dump(new PrintWriter(System.out, true));
 		blob.getCoreCount();
 	}
 }
