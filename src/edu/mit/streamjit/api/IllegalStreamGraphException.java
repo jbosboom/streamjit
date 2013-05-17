68bfcca01203b0f1823399b8d59815256035f23c
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-05-17 16:06:02 -0400

    IllegalStreamGraphException custom serialization

--- src/edu/mit/streamjit/api/IllegalStreamGraphException.java
+++ src/edu/mit/streamjit/api/IllegalStreamGraphException.java
@@ -1,11 +1,23 @@
 package edu.mit.streamjit.api;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Arrays;
 
 /**
  * Thrown when a stream graph is malformed.  This indicates a programming error
  * in the code that built the stream graph or in the implementation of one or
  * more StreamElements in the graph.
+ *
+ * IllegalStreamGraphException implements Serializable, but the StreamElements
+ * it refers to usually do not.  When an IllegalStreamGraphException is
+ * serialized, any StreamElements that do not implement Serializable are
+ * replaced with dummy StreamElement implementations that have the same
+ * toString() but without any other data or behavior.
  * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
  * @since 11/20/2012
  */
@@ -14,8 +26,10 @@ public final class IllegalStreamGraphException extends RuntimeException {
 	/**
 	 * A list of one or more stream elements involved in the malformation, to
 	 * aid in debuggging.
+	 *
+	 * Effectively final, but we have to make it non-final for serialization.
 	 */
-	private final StreamElement<?, ?>[] elements;
+	private transient StreamElement<?, ?>[] elements;
 
 	public IllegalStreamGraphException(Throwable cause) {
 		this(null, cause);
@@ -33,6 +47,10 @@ public final class IllegalStreamGraphException extends RuntimeException {
 	/**
 	 * Returns an array of elements involved in the malformation, to aid in
 	 * debugging.  May be a 0-length array.
+	 *
+	 * If this IllegalStreamGraphException was deserialized, the StreamElements
+	 * aren't useful for anything but their toString() method.  Calling other
+	 * methods (or passing them as parameters to methods) will throw exceptions.
 	 * @return an array of elements involved in the malformation
 	 */
 	public StreamElement<?, ?>[] getElements() {
@@ -48,4 +66,50 @@ public final class IllegalStreamGraphException extends RuntimeException {
 			return Arrays.toString(elements);
 		return msg + ": " + Arrays.toString(elements);
 	}
+
+	//<editor-fold defaultstate="collapsed" desc="Custom serialization">
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		oos.defaultWriteObject();
+		StreamElement<?, ?>[] serializableElements = new StreamElement<?, ?>[elements.length];
+		for (int i = 0; i < elements.length; ++i)
+			serializableElements[i] = (elements[i] instanceof Serializable) ? elements[i] : new DummyStreamElement(elements[i].toString());
+		oos.writeObject(serializableElements);
+	}
+
+	private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+		ois.defaultReadObject();
+		this.elements = (StreamElement<?, ?>[])ois.readObject();
+	}
+
+	private static final class DummyStreamElement implements StreamElement<Void, Void>, Serializable {
+		private static final long serialVersionUID = 1L;
+		private final String toString;
+		private DummyStreamElement(String toString) {
+			this.toString = toString;
+		}
+		@Override
+		public void visit(StreamVisitor v) {
+			throw new UnsupportedOperationException("Not a real StreamElement!");
+		}
+		@Override
+		public String toString() {
+			return toString;
+		}
+	}
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		StreamElement<?, ?> identity = new Identity<>();
+		IllegalStreamGraphException isge = new IllegalStreamGraphException("message", identity);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		oos.writeObject(isge);
+		ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+		ObjectInputStream ois = new ObjectInputStream(bais);
+		IllegalStreamGraphException ex = (IllegalStreamGraphException)ois.readObject();
+
+		ex.printStackTrace(System.out);
+		System.out.println(ex.getElements()[0].toString());
+		System.out.println(ex.getElements()[0].getClass());
+	}
+	//</editor-fold>
 }
