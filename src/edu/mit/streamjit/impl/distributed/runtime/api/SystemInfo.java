d8877bfe4c9859d1ee2348d53e8a280ee4f45b38
sumanan <sumanan@csail.mit.edu>
2013-06-05 21:04:26 -0400

    Comments added

--- src/edu/mit/streamjit/impl/distributed/runtime/api/SystemInfo.java
+++ src/edu/mit/streamjit/impl/distributed/runtime/api/SystemInfo.java
@@ -1,16 +1,26 @@
-/**
- * @author Sumanan sumanan@mit.edu
- * @since May 17, 2013
- */
 package edu.mit.streamjit.impl.distributed.runtime.api;
 
 
-public class SystemInfo implements MessageElement{
+/**
+ * {@link SystemInfo} holds the current system parameters such as CPU usage, memory usage and battery level. Note that {@link NodeInfo}
+ * , in contrast to {@link SystemInfo}, holds the computing node's hardware parameters such as IP address, human readable name, CPU
+ * cores, RAM size, etc.
+ * 
+ * @author Sumanan sumanan@mit.edu
+ * @since May 17, 2013
+ */
+public class SystemInfo implements MessageElement {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 626480245760997626L;
+	
 	public double cpuUsage;
 	public double memoryUsage;
 	public double baterryLevel;
+
 	@Override
 	public void accept(MessageVisitor visitor) {
 		visitor.visit(this);
-	}	
+	}
 }
