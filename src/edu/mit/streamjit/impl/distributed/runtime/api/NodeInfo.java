0e29228c8706b53e44fddc6b73d8d4a55341d67d
sumanan <sumanan@csail.mit.edu>
2013-06-05 21:51:21 -0400

    continuation of the previous change.

--- src/edu/mit/streamjit/impl/distributed/runtime/api/NodeInfo.java
+++ src/edu/mit/streamjit/impl/distributed/runtime/api/NodeInfo.java
@@ -1,12 +1,12 @@
 package edu.mit.streamjit.impl.distributed.runtime.api;
 
-import edu.mit.streamjit.impl.distributed.runtime.common.Ipv4Validator;
+import java.net.InetAddress;
 
 /**
  * {@link NodeInfo} is to store and pass the information about the nodes such as machines, servers, or mobile phones that are used to
  * execute the stream application. This informations may be needed by the slaves in order to establish connections with other dependent
  * computing nodes to successfully execute a stream application.
- *  
+ * 
  * @author Sumanan sumanan@mit.edu
  * @since May 7, 2013
  */
@@ -15,25 +15,23 @@ public class NodeInfo implements MessageElement {
 	private static final long serialVersionUID = -2627560179074739731L;
 
 	/**
-	 * Human convenient name of the computing node. e.g, ClusterNode7, SamsungS2, testPC, etc. For easy logging and error printing.
+	 * Human convenient hostName of the computing node. e.g, ClusterNode7, SamsungS2, testPC, etc. For easy logging and error printing.
 	 */
-	private String name;
+	private String hostName;
 
-	private String ipv4Address;
+	private InetAddress ipAddress;
 
 	private int availableCores;
 
 	/**
-	 * RAM size in MB.
+	 * RAM size in bytes.
 	 */
-	private int ramSize;
+	private long ramSize;
 
-	public NodeInfo(String name, String ipv4Address, int availableCores, int ramSize) {
-		if (!Ipv4Validator.getInstance().isValid(ipv4Address))
-			throw new IllegalArgumentException("Invalid IPV4 address");
+	public NodeInfo(String hostName, InetAddress ipAddress, int availableCores, long ramSize) {
 
-		this.name = name;
-		this.setIpv4Address(ipv4Address);
+		this.hostName = hostName;
+		this.ipAddress = ipAddress;
 		this.availableCores = availableCores;
 		this.ramSize = ramSize;
 	}
@@ -46,22 +44,7 @@ public class NodeInfo implements MessageElement {
 	 *            : The {@link NodeInfo} object which need to be copied/cloned.
 	 */
 	public NodeInfo(NodeInfo nodeinfo) {
-		this(nodeinfo.name, nodeinfo.ipv4Address, nodeinfo.availableCores, nodeinfo.ramSize);
-	}
-
-	/**
-	 * @return the name of human readable Name.
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
+		this(nodeinfo.hostName, nodeinfo.ipAddress, nodeinfo.availableCores, nodeinfo.ramSize);
 	}
 
 	/**
@@ -72,34 +55,24 @@ public class NodeInfo implements MessageElement {
 	}
 
 	/**
-	 * @param availableCores
-	 * 
+	 * @return the hostName of human readable Name.
 	 */
-	public void setAvailableCores(int availableCores) {
-		this.availableCores = availableCores;
+	public String getHostName() {
+		return hostName;
 	}
 
 	/**
-	 * @return RAM size in MB.
+	 * @return the ipAddress
 	 */
-	public int getRjamSize() {
+	public InetAddress getIpAddress() {
+		return ipAddress;
+	}
+
+	/**
+	 * @return RAM size in bytes
+	 */
+	public long getRamSize() {
 		return ramSize;
-	}
-
-	/**
-	 * @param ramSize
-	 *            : RAM size in MB.
-	 */
-	public void setRamSize(int ramSize) {
-		this.ramSize = ramSize;
-	}
-
-	public String getIpv4Address() {
-		return ipv4Address;
-	}
-
-	public void setIpv4Address(String ipv4Address) {
-		this.ipv4Address = ipv4Address;
 	}
 
 	@Override
--- src/edu/mit/streamjit/impl/distributed/runtime/slave/SlaveRequestProcessor.java
+++ src/edu/mit/streamjit/impl/distributed/runtime/slave/SlaveRequestProcessor.java
@@ -5,8 +5,10 @@
 package edu.mit.streamjit.impl.distributed.runtime.slave;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-import edu.mit.streamjit.impl.distributed.runtime.api.Request;
+import edu.mit.streamjit.impl.distributed.runtime.api.NodeInfo;
 import edu.mit.streamjit.impl.distributed.runtime.api.RequestProcessor;
 
 public class SlaveRequestProcessor implements RequestProcessor {
@@ -48,11 +50,28 @@ public class SlaveRequestProcessor implements RequestProcessor {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
 	}
 
 	@Override
 	public void processNodeInfo() {
-		
+
+		InetAddress localMachine;
+		String hostName;
+		int availableCores = Runtime.getRuntime().availableProcessors();
+		// TODO: Need to verify the ramSize.
+		long ramSize = Runtime.getRuntime().maxMemory();
+		try {
+			localMachine = InetAddress.getLocalHost();
+			hostName = localMachine.getHostName();
+			NodeInfo myInfo = new NodeInfo(hostName, localMachine, availableCores, ramSize);
+			slave.masterConnection.writeObject(myInfo);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
