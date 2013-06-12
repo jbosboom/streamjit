be06e52a7daecce4b4ad6bd548532319e09bcd38
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-06-12 12:09:52 -0400

    Scheduler can find initialization schedules

--- src/edu/mit/streamjit/impl/compiler/Scheduler.java
+++ src/edu/mit/streamjit/impl/compiler/Scheduler.java
@@ -32,11 +32,21 @@ public final class Scheduler {
 	public static class Channel<T> {
 		public final T inputThing, outputThing;
 		public final int inputRate, outputRate;
+		/**
+		 * The change in buffer size between these things after one execution of
+		 * this schedule.  For steady-state scheduling, this should be 0; for
+		 * initialization scheduling, this should be the desired buffering.
+		 */
+		public final int bufferDelta;
 		public Channel(T inputThing, T outputThing, int inputRate, int outputRate) {
+			this(inputThing, outputThing, inputRate, outputRate, 0);
+		}
+		public Channel(T inputThing, T outputThing, int inputRate, int outputRate, int bufferDelta) {
 			this.inputThing = checkNotNull(inputThing);
 			this.outputThing = checkNotNull(outputThing);
 			this.inputRate = inputRate;
 			this.outputRate = outputRate;
+			this.bufferDelta = bufferDelta;
 		}
 	}
 
@@ -57,26 +67,36 @@ public final class Scheduler {
 		}
 
 		Rational[][] matrixArray = new Rational[channels.size()][things];
+		Rational[] vectorArray = new Rational[channels.size()];
 		for (int i = 0; i < channels.size(); ++i) {
 			Arrays.fill(matrixArray[i], Rational.FACTORY.get(0));
 			Channel<T> c = channels.get(i);
 			matrixArray[i][thingIds.get(c.inputThing)] = Rational.FACTORY.get(c.inputRate);
 			matrixArray[i][thingIds.get(c.outputThing)] = Rational.FACTORY.get(-c.outputRate);
+			vectorArray[i] = Rational.FACTORY.get(c.bufferDelta);
 		}
 		Matrix<Rational> matrix = new Matrix<>(matrixArray);
-		Vector<Rational> vector = new Vector<>(Collections.nCopies(channels.size(), Rational.FACTORY.get(0)).toArray(new Rational[0]));
+		Vector<Rational> vector = new Vector<>(vectorArray);
 		AffineLinearSubspace<Rational> solutionSpace = LinSysSolver.solutionSpace(matrix, vector).normalize();
-		assert solutionSpace.getInhomogenousPart() == null;
+		System.out.println(solutionSpace);
+		if (vector.isZero())
+			assert solutionSpace.getInhomogenousPart() == null;
 		assert solutionSpace.getDimension() == 1;
 
+		Vector<Rational> homogeneous = solutionSpace.getGeneratingSystem()[0];
+		if (homogeneous.getEntry(1).compareTo(Rational.FACTORY.zero()) < 0)
+			homogeneous = homogeneous.multiply(Rational.FACTORY.get(-1));
 		Vector<Rational> v = solutionSpace.getGeneratingSystem()[0];
-		if (v.getEntry(1).compareTo(Rational.FACTORY.zero()) < 0)
-			v.multiplyReplace(Rational.FACTORY.get(-1));
-		for (int i = 1; i <= v.length(); ++i) {
-			BigInteger d = v.getEntry(i).getDenominator();
-			if (!d.equals(BigInteger.ONE))
-				v.multiplyReplace(Rational.FACTORY.get(d));
-		}
+		if (solutionSpace.getInhomogenousPart() != null)
+			v = v.add(solutionSpace.getInhomogenousPart());
+		while (!validSchedule(v))
+			v = v.add(homogeneous);
+//		for (int i = 1; i <= v.length(); ++i) {
+//			BigInteger d = v.getEntry(i).getDenominator();
+//			if (!d.equals(BigInteger.ONE))
+//				v.multiplyReplace(Rational.FACTORY.get(d));
+//		}
+
 		System.out.println(v);
 
 		ImmutableMap.Builder<T, Integer> retval = ImmutableMap.<T, Integer>builder();
@@ -90,10 +110,17 @@ public final class Scheduler {
 		return retval.build();
 	}
 
+	private static boolean validSchedule(Vector<Rational> v) {
+		for (int i = 1; i < v.length(); ++i)
+			if (!v.getEntry(i).getDenominator().equals(BigInteger.ONE) || v.getEntry(i).getNumerator().signum() <= 0)
+				return false;
+		return true;
+	}
+
 	public static void main(String[] args) {
 		Object a = new Object(), b = new Object(), c = new Object(), d = new Object();
 		Channel<Object> channel1 = new Channel<>(a, b, 3, 2);
-		Channel<Object> channel2 = new Channel<>(a, c, 5, 7);
+		Channel<Object> channel2 = new Channel<>(a, c, 5, 7, 2);
 		Channel<Object> channel3 = new Channel<>(b, d, 3, 1);
 		ImmutableMap<Object, Integer> schedule = schedule(Arrays.asList(channel1, channel2, channel3));
 		System.out.println(schedule);
