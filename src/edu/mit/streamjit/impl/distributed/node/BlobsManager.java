84c7aa42aa89dbe86df7dc053fc5881ec6ef08a1
sumanan <sumanan@csail.mit.edu>
2013-07-25 14:51:26 -0400

    Comments modified.

--- src/edu/mit/streamjit/impl/distributed/node/BlobsManager.java
+++ src/edu/mit/streamjit/impl/distributed/node/BlobsManager.java
@@ -1,11 +1,10 @@
 package edu.mit.streamjit.impl.distributed.node;
 
 /**
- * BlobsManager is the main dispatcher for all blobs. Received commands will
- * call the appropriate {@link BlobsManager}'s functions and the functions will
- * take responsibility execute the command on all assigned {@link Blob}s through
- * {@link BlobExecuter}
- * 
+ * {@link BlobsManager} is the main dispatcher for all blobs. Received commands
+ * will call the appropriate BlobsManager's functions and the BlobsManager will
+ * take responsibility to execute the command on all assigned {@link Blob}s.
+ * </p>
  * TODO: Draining mechanism need to be added.
  * 
  * @author Sumanan sumanan@mit.edu
