131bbcb77526a04792de2b6c61f5fe5cc43e76c5
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-08-07 15:44:52 -0400

    Capture logging by passing a logfunc
    Unfortunately lpsolve takes the function by value rather than by
    pointer; this is equivalent in C but confuses BridJ, which can't
    see that it's just a pointer-to-function.
    
    There may be a workaround, but it's not worth spending more time on.

--- src/edu/mit/streamjit/util/ilpsolve/ILPSolver.java
+++ src/edu/mit/streamjit/util/ilpsolve/ILPSolver.java
@@ -22,6 +22,13 @@ public final class ILPSolver {
 	private final List<Constraint> constraints = new ArrayList<>();
 	private ObjectiveFunction objFn = null;
 	private boolean solved = false;
+	private final StringBuilder log = new StringBuilder();
+	private final LogFunction logFn = new LogFunction() {
+		@Override
+		public void apply(Pointer<lprec> lp, Pointer<?> userhandle, Pointer<Byte> buf) {
+			log.append(buf.getCString());
+		}
+	};
 	public ILPSolver() {}
 
 	public Variable newVariable() {
@@ -71,6 +78,10 @@ public final class ILPSolver {
 		return objFn;
 	}
 
+	public String output() {
+		return log.toString();
+	}
+
 	public void solve() {
 		checkState(!solved, "system already solved");
 		checkState(objFn != null, "objective function not set");
@@ -98,6 +109,7 @@ public final class ILPSolver {
 			} else {
 				setVerbose(lp, IMPORTANT);
 			}
+			putLogfunc(lp, logFn, Pointer.NULL);
 
 			setAddRowmode(lp, (byte)1);
 			storeCoefficientsInRow(objFn.expr, row);
