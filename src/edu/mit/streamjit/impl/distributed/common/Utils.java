b960b7c88551f9b7a678eb01ce900a2ba78df37e
sumanan <sumanan@csail.mit.edu>
2013-08-15 12:04:23 -0400

    overloaded method getblobID() has been added.

--- src/edu/mit/streamjit/impl/distributed/common/Utils.java
+++ src/edu/mit/streamjit/impl/distributed/common/Utils.java
@@ -1,9 +1,14 @@
 package edu.mit.streamjit.impl.distributed.common;
 
 import java.util.Collections;
+import java.util.Set;
 
+import com.google.common.collect.ImmutableSet;
+
+import edu.mit.streamjit.api.Worker;
 import edu.mit.streamjit.impl.blob.Blob;
 import edu.mit.streamjit.impl.blob.Blob.Token;
+import edu.mit.streamjit.impl.common.IOInfo;
 
 /**
  * @author Sumanan sumanan@mit.edu
@@ -13,5 +18,15 @@ public class Utils {
 
 	public static Token getBlobID(Blob b) {
 		return Collections.min(b.getInputs());
+	}
+
+	public static Token getblobID(Set<Worker<?, ?>> workers) {
+		ImmutableSet.Builder<Token> inputBuilder = new ImmutableSet.Builder<>();
+		for (IOInfo info : IOInfo.externalEdges(workers)) {
+			if (info.isInput())
+				inputBuilder.add(info.token());
+		}
+
+		return Collections.min(inputBuilder.build());
 	}
 }
