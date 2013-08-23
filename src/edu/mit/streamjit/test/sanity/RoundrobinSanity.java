f0dd68e7efd3913702208a27ad071cbc009df50c
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-08-23 00:15:11 -0400

    RoundrobinSanity is a BenchmarkProvider
    Now we can set many different parameterizations quickly.

--- src/edu/mit/streamjit/test/sanity/RoundrobinSanity.java
+++ src/edu/mit/streamjit/test/sanity/RoundrobinSanity.java
@@ -1,21 +1,26 @@
 package edu.mit.streamjit.test.sanity;
 
 import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableList;
 import com.jeffreybosboom.serviceproviderprocessor.ServiceProvider;
 import edu.mit.streamjit.api.Identity;
 import edu.mit.streamjit.api.Input;
+import edu.mit.streamjit.api.Joiner;
 import edu.mit.streamjit.api.RoundrobinJoiner;
 import edu.mit.streamjit.api.RoundrobinSplitter;
 import edu.mit.streamjit.api.Splitjoin;
+import edu.mit.streamjit.api.Splitter;
 import edu.mit.streamjit.impl.blob.Buffer;
 import edu.mit.streamjit.impl.common.InputBufferFactory;
 import edu.mit.streamjit.test.AbstractBenchmark;
 import edu.mit.streamjit.test.Benchmark;
 import edu.mit.streamjit.test.Benchmark.Dataset;
+import edu.mit.streamjit.test.BenchmarkProvider;
 import edu.mit.streamjit.test.Datasets;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Queue;
 
@@ -24,54 +29,68 @@ import java.util.Queue;
  * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
  * @since 8/20/2013
  */
-public class RoundrobinSanity {
-	@ServiceProvider(Benchmark.class)
-	public static final class RoundrobinReorderSanity extends AbstractBenchmark {
-		public RoundrobinReorderSanity() {
-			super("RoundrobinReorderSanity", new Supplier<Splitjoin<Integer, Integer>>() {
-					@Override
-					public Splitjoin<Integer, Integer> get() {
-						return new Splitjoin<>(
-								new RoundrobinSplitter<Integer>(),
-								new RoundrobinJoiner<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>());
-					}
-				},
-					simulateRoundrobin(Datasets.allIntsInRange(0, 1_000_000), 7, 1, 1),
-					simulateRoundrobin(Datasets.nCopies(100, "STRING"), 7, 1, 1));
-		}
-	}
-
-	@ServiceProvider(Benchmark.class)
-	public static final class RoundrobinReorder5Sanity extends AbstractBenchmark {
-		public RoundrobinReorder5Sanity() {
-			super("RoundrobinReorder5Sanity", new Supplier<Splitjoin<Integer, Integer>>() {
-					@Override
-					public Splitjoin<Integer, Integer> get() {
-						return new Splitjoin<>(
-								new RoundrobinSplitter<Integer>(5),
-								new RoundrobinJoiner<Integer>(5),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>(),
-								new Identity<Integer>());
-					}
-				},
-					simulateRoundrobin(Datasets.allIntsInRange(0, 1_000_000), 7, 5, 5),
-					simulateRoundrobin(Datasets.nCopies(100, "STRING"), 7, 5, 5));
-		}
-	}
-
+@ServiceProvider(BenchmarkProvider.class)
+public final class RoundrobinSanity implements BenchmarkProvider {
 	//TODO: a split 5 join 3 test (will actually reorder elements)
+
+	@Override
+	public Iterator<Benchmark> iterator() {
+		Benchmark[] benchmarks = {
+			rr_rr(7, 1, 1),
+			rr_rr(7, 5, 5),
+		};
+		return Arrays.asList(benchmarks).iterator();
+	}
+
+	private static Benchmark rr_rr(int width, int splitRate, int joinRate) {
+		String name = String.format("RR(%d) x %dw x RR(%d)", splitRate, width, joinRate);
+		return new AbstractBenchmark(name,
+				new SplitjoinSupplier(width, new RoundrobinSplitterSupplier(splitRate), new RoundrobinJoinerSupplier(joinRate)),
+				simulateRoundrobin(Datasets.allIntsInRange(0, 1_000_000), width, splitRate, joinRate));
+	}
+
+	private static final class SplitjoinSupplier implements Supplier<Splitjoin<Integer, Integer>> {
+		private final int width;
+		private final Supplier<? extends Splitter<Integer, Integer>> splitter;
+		private final Supplier<? extends Joiner<Integer, Integer>> joiner;
+		private SplitjoinSupplier(int width, Supplier<? extends Splitter<Integer, Integer>> splitter, Supplier<? extends Joiner<Integer, Integer>> joiner) {
+			this.width = width;
+			this.splitter = splitter;
+			this.joiner = joiner;
+		}
+		@Override
+		public Splitjoin<Integer, Integer> get() {
+			ImmutableList.Builder<Identity<Integer>> builder = ImmutableList.builder();
+			//Can't use Collections.nCopies because we need distinct filters.
+			for (int i = 0; i < width; ++i)
+				builder.add(new Identity<Integer>());
+			return new Splitjoin<>(splitter.get(), joiner.get(), builder.build());
+		}
+	}
+
+	//I'd like to use ConstructorSupplier here, but the generics won't work
+	//because e.g. RoundrobinSplitter.class is a raw type.
+	private static final class RoundrobinSplitterSupplier implements Supplier<Splitter<Integer, Integer>> {
+		private final int rate;
+		private RoundrobinSplitterSupplier(int rate) {
+			this.rate = rate;
+		}
+		@Override
+		public Splitter<Integer, Integer> get() {
+			return new RoundrobinSplitter<>(rate);
+		}
+	}
+
+	private static final class RoundrobinJoinerSupplier implements Supplier<Joiner<Integer, Integer>> {
+		private final int rate;
+		private RoundrobinJoinerSupplier(int rate) {
+			this.rate = rate;
+		}
+		@Override
+		public Joiner<Integer, Integer> get() {
+			return new RoundrobinJoiner<>(rate);
+		}
+	}
 
 	/**
 	 * Simulates a roundrobin splitjoin, returning a Dataset with reference
