8c514ebd9294cd8e7317fa98db07c09495543d29
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-08-26 20:11:10 -0400

    Datasets: nCopies on Input
    This is useful for file inputs that aren't long enough for much
    steady-state execution.

--- src/edu/mit/streamjit/test/Datasets.java
+++ src/edu/mit/streamjit/test/Datasets.java
@@ -4,6 +4,9 @@ import com.google.common.collect.ContiguousSet;
 import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.Range;
 import edu.mit.streamjit.api.Input;
+import edu.mit.streamjit.impl.blob.AbstractReadOnlyBuffer;
+import edu.mit.streamjit.impl.blob.Buffer;
+import edu.mit.streamjit.impl.common.InputBufferFactory;
 import edu.mit.streamjit.test.Benchmark.Dataset;
 import java.util.Collections;
 
@@ -29,5 +32,32 @@ public final class Datasets {
 
 	public static Dataset allIntsInRange(Range<Integer> range) {
 		return fromIterable(range.toString(), ContiguousSet.create(range, DiscreteDomain.integers()));
+	}
+
+	public static <I> Input<I> nCopies(final int n, final Input<I> input) {
+		return InputBufferFactory.wrap(new InputBufferFactory() {
+			@Override
+			public Buffer createReadableBuffer(int readerMinSize) {
+				final Buffer first = InputBufferFactory.unwrap(input).createReadableBuffer(readerMinSize);
+				final int initialSize = first.size()*n;
+				return new AbstractReadOnlyBuffer() {
+					private Buffer currentBuffer = first;
+					private int size = initialSize;
+					@Override
+					public Object read() {
+						if (size == 0)
+							return null;
+						if (currentBuffer.size() == 0)
+							currentBuffer = InputBufferFactory.unwrap(input).createReadableBuffer(initialSize);
+						--size;
+						return currentBuffer.read();
+					}
+					@Override
+					public int size() {
+						return size;
+					}
+				};
+			}
+		});
 	}
 }
