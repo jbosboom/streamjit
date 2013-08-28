1da30b330297233da0a438d95d9da26b1738635f
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-08-27 21:29:21 -0400

    Datasets: some functional Input implementations

--- src/edu/mit/streamjit/test/Datasets.java
+++ src/edu/mit/streamjit/test/Datasets.java
@@ -1,7 +1,11 @@
 package edu.mit.streamjit.test;
 
+import com.google.common.base.Function;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.google.common.collect.ContiguousSet;
 import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Range;
 import edu.mit.streamjit.api.Input;
 import edu.mit.streamjit.impl.blob.AbstractReadOnlyBuffer;
@@ -55,6 +59,41 @@ public final class Datasets {
 					@Override
 					public int size() {
 						return size;
+					}
+				};
+			}
+		});
+	}
+
+	public static <I> Input<I> lazyInput(Supplier<? extends Input<? extends I>> supplier) {
+		final Supplier<? extends Input<? extends I>> memoized = Suppliers.memoize(supplier);
+		return InputBufferFactory.wrap(new InputBufferFactory() {
+			@Override
+			public Buffer createReadableBuffer(int readerMinSize) {
+				return unwrap(memoized.get()).createReadableBuffer(readerMinSize);
+			}
+		});
+	}
+
+	public static <I, J> Input<J> transformAll(Function<Input<? super I>, Input<? extends J>> function, Input<I> input) {
+		return lazyInput(Suppliers.compose(function, Suppliers.ofInstance(input)));
+	}
+
+	public static <I, J> Input<J> transformOne(final Function<? super I, ? extends J> function, final Input<I> input) {
+		return InputBufferFactory.wrap(new InputBufferFactory() {
+			@Override
+			public Buffer createReadableBuffer(final int readerMinSize) {
+				return new AbstractReadOnlyBuffer() {
+					private final Buffer delegate = unwrap(input).createReadableBuffer(readerMinSize);
+					@Override
+					@SuppressWarnings("unchecked")
+					public Object read() {
+						//This is safe because an Input<I>'s buffer contains Is.
+						return function.apply((I)delegate.read());
+					}
+					@Override
+					public int size() {
+						return delegate.size();
 					}
 				};
 			}
