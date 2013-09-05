d2ce000039d95a3879210838f133c67f57d28f0b
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-09-04 20:18:10 -0400

    First fuzzer regression test
    This one's simple enough that it could be a sanity test, I guess.

---
+++ src/edu/mit/streamjit/test/regression/Reg20130904_081439_761.java
@@ -0,0 +1,36 @@
+package edu.mit.streamjit.test.regression;
+
+import com.jeffreybosboom.serviceproviderprocessor.ServiceProvider;
+import edu.mit.streamjit.api.OneToOneElement;
+import edu.mit.streamjit.api.Pipeline;
+import edu.mit.streamjit.api.Splitjoin;
+import edu.mit.streamjit.test.Benchmark;
+import edu.mit.streamjit.test.Benchmarker;
+import edu.mit.streamjit.test.Datasets;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @since 9/4/2013 8:14PM EDT
+ */
+@ServiceProvider(Benchmark.class)
+public class Reg20130904_081439_761 implements Benchmark {
+	@Override
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	public OneToOneElement<Object, Object> instantiate() {
+		return new Pipeline(new edu.mit.streamjit.impl.common.TestFilters.Batcher(2), new edu.mit.streamjit.impl.common.TestFilters.Batcher(10), new edu.mit.streamjit.impl.common.TestFilters.Batcher(2));
+	}
+	@Override
+	public List<Dataset> inputs() {
+		Dataset ds = Datasets.allIntsInRange(0, 1000);
+		return Collections.singletonList(ds.withOutput(Datasets.outputOf(new edu.mit.streamjit.impl.interp.InterpreterStreamCompiler(), instantiate(), ds.input())));
+	}
+	@Override
+	public String toString() {
+		return getClass().getSimpleName();
+	}
+	public static void main(String[] args) {
+		Benchmarker.runBenchmark(new Reg20130904_081439_761(), new edu.mit.streamjit.impl.compiler.CompilerStreamCompiler()).get(0).print(System.out);
+	}
+}
+
