4824b929bfa7a18a9f7796a9b93ad17909feeb56
sumanan <sumanan@csail.mit.edu>
2013-09-04 21:04:20 -0400

    Switch parameter has been added.

--- lib/opentuner/streamjit/sjparameters.py
+++ lib/opentuner/streamjit/sjparameters.py
@@ -1,7 +1,7 @@
 import deps #fix sys.path
 import opentuner
 from opentuner.search.manipulator import (IntegerParameter,
-                                          FloatParameter)
+                                          FloatParameter, SwitchParameter)
 
 class sjIntegerParameter(IntegerParameter):
 	def __init__(self, name, min, max,value, javaClassPath = None, **kwargs):
@@ -25,9 +25,31 @@ class sjFloatParameter(FloatParameter):
 		return self.value	
 
 	def getJavaClassPath(self):
-		return self.javaClassPath		
+		return self.javaClassPath
+
+class sjSwitchParameter(SwitchParameter):
+	def __init__(self, name, universeType, universe,value, javaClassPath = None, **kwargs):
+		self.value = value
+		self.javaClassPath = javaClassPath
+		self.universeType = universeType
+		self.universe = universe
+		super(sjSwitchParameter, self).__init__(name, len(universe), **kwargs)
+
+	def getValue(self):
+		return self.value
+
+	def getJavaClassPath(self):
+		return self.javaClassPath
+
+	def getUniverse(self):
+		return self.universe
+
+	def getUniverseType(self):
+		return self.universeType
 
 if __name__ == '__main__':
 	ip = IntegerParameter("suman", 2, 7)
 	sjip = sjIntegerParameter("ss", 3, 56, 45)
+	sjsw = sjSwitchParameter('sjswtch', 'java.lang.Integer', [1, 2, 3, 4], 2, 'edu.mit.streamjit.impl.common.Configuration$SwitchParameter')
+	print sjsw.getUniverse()
 	print sjip.getValue()
