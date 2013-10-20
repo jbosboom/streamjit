0e8b0bdcd9c73409ef73cf7a768ff9062203b352
sumanan <sumanan@csail.mit.edu>
2013-10-20 14:28:24 -0400

    Two new SNExceptions have been added

--- src/edu/mit/streamjit/impl/distributed/common/SNException.java
+++ src/edu/mit/streamjit/impl/distributed/common/SNException.java
@@ -1,10 +1,47 @@
 package edu.mit.streamjit.impl.distributed.common;
 
-public class SNException implements SNMessageElement{
+import edu.mit.streamjit.impl.blob.Blob.Token;
+import edu.mit.streamjit.impl.distributed.common.TCPConnection.TCPConnectionInfo;
+
+public class SNException implements SNMessageElement {
+
+	private static final long serialVersionUID = 1L;
+
+	public void process(SNExceptionProcessor exP) {
+		exP.process(this);
+	}
 
 	@Override
 	public void accept(SNMessageVisitor visitor) {
 		visitor.visit(this);
 	}
 
+	public static final class AddressBindException extends SNException {
+		private static final long serialVersionUID = 1L;
+
+		public final TCPConnectionInfo conInfo;
+
+		public AddressBindException(TCPConnectionInfo conInfo) {
+			this.conInfo = conInfo;
+		}
+	}
+
+	public static final class MakeBlobException extends SNException {
+		private static final long serialVersionUID = 1L;
+
+		public final Token blobID;
+
+		public MakeBlobException(Token blobID) {
+			this.blobID = blobID;
+		}
+	}
+
+	public interface SNExceptionProcessor {
+
+		public void process(SNException ex);
+
+		public void process(AddressBindException abEx);
+
+		public void process(MakeBlobException mbEx);
+	}
 }
