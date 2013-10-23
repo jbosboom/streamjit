ae6ae52a8c8c0c9179650f4d9e0989fc984c2d60
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-10-22 20:19:57 -0400

    CollectionUtils.union()

---
+++ src/edu/mit/streamjit/util/CollectionUtils.java
@@ -0,0 +1,31 @@
+package edu.mit.streamjit.util;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+
+/**
+ * Contains collection related utilities not in {@link java.util.Collections}
+ * or the Guava collection utilities.
+ * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
+ * @since 10/22/2013
+ */
+public final class CollectionUtils {
+	private CollectionUtils() {}
+
+	/**
+	 * Returns the union of the given maps with disjoint key sets.
+	 * @param <K> the key type of the returned map
+	 * @param <V> the value type of the returned map
+	 * @param first the first map
+	 * @param more more maps
+	 * @return a map containing all the entries in the given maps
+	 */
+	@SafeVarargs
+	public static <K, V> ImmutableMap<K, V> union(Map<? extends K, ? extends V> first, Map<? extends K, ? extends V>... more) {
+		ImmutableMap.Builder<K, V> builder = ImmutableMap.builder();
+		builder.putAll(first);
+		for (Map<? extends K, ? extends V> m : more)
+			builder.putAll(m);
+		return builder.build();
+	}
+}
