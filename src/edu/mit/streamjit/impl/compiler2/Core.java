25f69a757d467432af670003f08538a27f010c83
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-10-28 20:01:56 -0400

    Core (collects ActorGroup allocations into MH)

---
+++ src/edu/mit/streamjit/impl/compiler2/Core.java
@@ -0,0 +1,54 @@
+package edu.mit.streamjit.impl.compiler2;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Range;
+import edu.mit.streamjit.util.CollectionUtils;
+import edu.mit.streamjit.util.Combinators;
+import edu.mit.streamjit.util.Pair;
+import java.lang.invoke.MethodHandle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Represents one core during the compilation.
+ * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
+ * @since 10/17/2013
+ */
+public class Core {
+	private final ImmutableMap<Storage, ConcreteStorage> globalStorage, localStorage, allStorage;
+	private final List<Pair<ActorGroup, Range<Integer>>> allocations = new ArrayList<>();
+	public Core(Set<Storage> storage, ImmutableMap<Storage, ConcreteStorage> globalStorage, StorageFactory localStorageFactory) {
+		this.globalStorage = globalStorage;
+		//We make ConcreteStorage for every local storage, despite not knowing
+		//what we need yet; anything we don't use will get garbage collected
+		//when the compiler state is discarded after instantiating the Blob.
+		ImmutableMap.Builder<Storage, ConcreteStorage> localStorageBuilder = ImmutableMap.builder();
+		for (Storage s : storage)
+			if (s.isInternal())
+				localStorageBuilder.put(s, localStorageFactory.make(s));
+		this.localStorage = localStorageBuilder.build();
+		this.allStorage = CollectionUtils.union(globalStorage, localStorage);
+	}
+
+	public void allocate(ActorGroup group, Range<Integer> iterations) {
+		allocations.add(Pair.make(group, iterations));
+	}
+
+	public MethodHandle code() {
+		//TODO: here's where to plug in ActorGroup ordering parameters
+		List<MethodHandle> code = new ArrayList<>(allocations.size());
+		for (Pair<ActorGroup, Range<Integer>> p : allocations)
+			code.add(p.first.specialize(p.second, allStorage));
+		return Combinators.semicolon(code);
+	}
+
+	/**
+	 * Returns the ConcreteStorage instances allocated for internal Storage by
+	 * this core.
+	 * @return local ConcreteStorage instances
+	 */
+	public ImmutableMap<Storage, ConcreteStorage> localStorage() {
+		return allStorage;
+	}
+}
