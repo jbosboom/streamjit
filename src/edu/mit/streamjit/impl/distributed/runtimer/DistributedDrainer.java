16e12f0216a3ce68be5b429acf3cbbc1e8ec5ce4
sumanan <sumanan@csail.mit.edu>
2013-11-03 15:30:10 -0500

    Unnecessary import removed

--- src/edu/mit/streamjit/impl/distributed/runtimer/DistributedDrainer.java
+++ src/edu/mit/streamjit/impl/distributed/runtimer/DistributedDrainer.java
@@ -3,7 +3,6 @@ package edu.mit.streamjit.impl.distributed.runtimer;
 import edu.mit.streamjit.impl.blob.Blob.Token;
 import edu.mit.streamjit.impl.common.AbstractDrainer;
 import edu.mit.streamjit.impl.distributed.StreamJitAppManager;
-import edu.mit.streamjit.impl.distributed.common.SNDrainElement.SNDrainProcessor;
 
 /**
  * @author Sumanan sumanan@mit.edu
