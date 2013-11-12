d6240990cebb401ef4c531fb95a01b0a1b159f88
sumanan <sumanan@csail.mit.edu>
2013-11-11 23:37:12 -0500

    Changes due to previous restructure
    Inner classes are changed to access outer class's instance variables and
    methods.

--- src/edu/mit/streamjit/impl/distributed/node/BlobsManagerImpl.java
+++ src/edu/mit/streamjit/impl/distributed/node/BlobsManagerImpl.java
@@ -58,8 +58,8 @@ public class BlobsManagerImpl implements BlobsManager {
 		this.streamNode = streamNode;
 		this.conProvider = conProvider;
 
-		this.cmdProcessor = new CommandProcessorImpl(streamNode);
-		this.drainProcessor = new CTRLRDrainProcessorImpl(streamNode);
+		this.cmdProcessor = new CommandProcessorImpl();
+		this.drainProcessor = new CTRLRDrainProcessorImpl();
 
 		bufferMap = createBufferMap(blobSet);
 
@@ -481,23 +481,16 @@ public class BlobsManagerImpl implements BlobsManager {
 	 * @author Sumanan sumanan@mit.edu
 	 * @since Jul 30, 2013
 	 */
-	public class CTRLRDrainProcessorImpl implements CTRLRDrainProcessor {
-
-		StreamNode streamNode;
-
-		public CTRLRDrainProcessorImpl(StreamNode streamNode) {
-			this.streamNode = streamNode;
-		}
+	private class CTRLRDrainProcessorImpl implements CTRLRDrainProcessor {
 
 		@Override
 		public void process(DrainDataRequest drnDataReq) {
-			streamNode.getBlobsManager().reqDrainedData(drnDataReq.blobsSet);
+			reqDrainedData(drnDataReq.blobsSet);
 		}
 
 		@Override
 		public void process(DoDrain drain) {
-			streamNode.getBlobsManager()
-					.drain(drain.blobID, drain.reqDrainData);
+			drain(drain.blobID, drain.reqDrainData);
 		}
 	}
 
@@ -507,59 +500,35 @@ public class BlobsManagerImpl implements BlobsManager {
 	 * @author Sumanan sumanan@mit.edu
 	 * @since May 27, 2013
 	 */
-	public class CommandProcessorImpl implements CommandProcessor {
-		StreamNode streamNode;
-
-		public CommandProcessorImpl(StreamNode streamNode) {
-			this.streamNode = streamNode;
-		}
+	private class CommandProcessorImpl implements CommandProcessor {
 
 		@Override
 		public void processSTART() {
-			BlobsManager bm = streamNode.getBlobsManager();
-			if (bm != null) {
-				bm.start();
-				long heapMaxSize = Runtime.getRuntime().maxMemory();
-				long heapSize = Runtime.getRuntime().totalMemory();
-				long heapFreeSize = Runtime.getRuntime().freeMemory();
+			start();
+			long heapMaxSize = Runtime.getRuntime().maxMemory();
+			long heapSize = Runtime.getRuntime().totalMemory();
+			long heapFreeSize = Runtime.getRuntime().freeMemory();
 
-				System.out
-						.println("##############################################");
+			System.out
+					.println("##############################################");
 
-				System.out.println("heapMaxSize = " + heapMaxSize / 1e6);
-				System.out.println("heapSize = " + heapSize / 1e6);
-				System.out.println("heapFreeSize = " + heapFreeSize / 1e6);
-				System.out.println("StraemJit app is running...");
-				System.out
-						.println("##############################################");
+			System.out.println("heapMaxSize = " + heapMaxSize / 1e6);
+			System.out.println("heapSize = " + heapSize / 1e6);
+			System.out.println("heapFreeSize = " + heapFreeSize / 1e6);
+			System.out.println("StraemJit app is running...");
+			System.out
+					.println("##############################################");
 
-			} else {
-				// TODO: Need to handle this case. Need to send the error
-				// message to
-				// the controller.
-				System.out
-						.println("Couldn't start the blobs...BlobsManager is null.");
-			}
 		}
 
 		@Override
 		public void processSTOP() {
-			BlobsManager bm = streamNode.getBlobsManager();
-			if (bm != null) {
-				bm.stop();
-				System.out.println("StraemJit app stopped...");
-				try {
-					streamNode.controllerConnection
-							.writeObject(AppStatus.STOPPED);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			} else {
-				// TODO: Need to handle this case. Need to send the error
-				// message to
-				// the controller.
-				System.out
-						.println("Couldn't stop the blobs...BlobsManager is null.");
+			stop();
+			System.out.println("StraemJit app stopped...");
+			try {
+				streamNode.controllerConnection.writeObject(AppStatus.STOPPED);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 
@@ -569,5 +538,4 @@ public class BlobsManagerImpl implements BlobsManager {
 			streamNode.exit();
 		}
 	}
-
 }
