29688f5e4ce9ea64d0756567d5dc9f91570cb2e7
sumanan <sumanan@csail.mit.edu>
2013-11-19 10:14:56 -0500

    Performance counter thread has been added
    This thread can be started when there is no OpenTuner and Streamjit
    application is set to run with a static configuration file. We need this
    feature to evaluate the performance of either hand tuned configuration or final
    configuration that is given by Opentuner at the end of the tuning.

--- src/edu/mit/streamjit/impl/distributed/TailChannel.java
+++ src/edu/mit/streamjit/impl/distributed/TailChannel.java
@@ -1,6 +1,12 @@
 package edu.mit.streamjit.impl.distributed;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
 
 import edu.mit.streamjit.impl.blob.Buffer;
 import edu.mit.streamjit.impl.distributed.common.TCPConnection.TCPConnectionInfo;
@@ -13,7 +19,7 @@ public class TailChannel extends TCPInputChannel {
 
 	int count;
 
-	CountDownLatch latch;
+	private volatile CountDownLatch latch;
 
 	public TailChannel(Buffer buffer, TCPConnectionProvider conProvider,
 			TCPConnectionInfo conInfo, String bufferTokenName, int debugPrint,
@@ -22,6 +28,7 @@ public class TailChannel extends TCPInputChannel {
 		this.limit = limit;
 		count = 0;
 		latch = new CountDownLatch(1);
+		// new performanceLogger().start();
 	}
 
 	@Override
@@ -41,5 +48,45 @@ public class TailChannel extends TCPInputChannel {
 		latch.countDown();
 		latch = new CountDownLatch(1);
 		count = 0;
+	}
+
+	private class performanceLogger extends Thread {
+
+		public void run() {
+			int i = 0;
+			FileWriter writer;
+			try {
+				writer = new FileWriter("1Interpreter.txt");
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				return;
+			}
+			while (++i < 30) {
+				try {
+					Stopwatch stopwatch = new Stopwatch();
+					stopwatch.start();
+					latch.await();
+					stopwatch.stop();
+					Long time = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+
+					// System.out.println("Execution time is " + time
+					// + " milli seconds");
+
+					writer.write(time.toString());
+					writer.write('\n');
+
+					reset();
+
+				} catch (InterruptedException | IOException e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
