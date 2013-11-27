24da33a1e1dc84ef6938a086f4cc3241915408ee
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2013-11-26 19:49:37 -0500

    ConstructorSupplier uses TypeToken
    This provides better support for creating instances of generic types.

--- src/edu/mit/streamjit/util/ConstructorSupplier.java
+++ src/edu/mit/streamjit/util/ConstructorSupplier.java
@@ -3,7 +3,8 @@ package edu.mit.streamjit.util;
 import com.google.common.base.Joiner;
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableList;
-import java.lang.reflect.Constructor;
+import com.google.common.reflect.Invokable;
+import com.google.common.reflect.TypeToken;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Arrays;
@@ -14,26 +15,44 @@ import java.util.Objects;
  * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
  * @since 8/20/2013
  */
-public final class ConstructorSupplier<T> implements Supplier<T> {
-	private final Constructor<T> ctor;
+public class ConstructorSupplier<T> implements Supplier<T> {
+	private final Invokable<T, T> ctor;
 	private final ImmutableList<?> arguments;
-	public ConstructorSupplier(Class<T> klass, Iterable<?> arguments) {
+	public ConstructorSupplier(TypeToken<T> type, Iterable<?> arguments) {
 		this.arguments = ImmutableList.copyOf(arguments);
 		try {
-			this.ctor = ReflectionUtils.findConstructor(klass, this.arguments);
+			this.ctor = type.constructor(ReflectionUtils.findConstructor(type.getRawType(), this.arguments));
 		} catch (NoSuchMethodException ex) {
 			throw new UndeclaredThrowableException(ex);
 		}
 	}
+	public ConstructorSupplier(TypeToken<T> type, Object... arguments) {
+		this(type, Arrays.asList(arguments));
+	}
+	public ConstructorSupplier(Class<T> klass, Iterable<?> arguments) {
+		this(TypeToken.of(klass), arguments);
+	}
 	public ConstructorSupplier(Class<T> klass, Object... arguments) {
 		this(klass, Arrays.asList(arguments));
+	}
+	protected ConstructorSupplier(Iterable<?> arguments) {
+		TypeToken<T> type = new TypeToken<T>(getClass()) {};
+		this.arguments = ImmutableList.copyOf(arguments);
+		try {
+			this.ctor = type.constructor(ReflectionUtils.findConstructor(type.getRawType(), this.arguments));
+		} catch (NoSuchMethodException ex) {
+			throw new UndeclaredThrowableException(ex);
+		}
+	}
+	protected ConstructorSupplier(Object... arguments) {
+		this(Arrays.asList(arguments));
 	}
 
 	@Override
 	public T get() {
 		try {
-			return ctor.newInstance(arguments.toArray());
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+			return ctor.invoke(null, arguments.toArray());
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
 			throw new UndeclaredThrowableException(ex);
 		}
 	}
