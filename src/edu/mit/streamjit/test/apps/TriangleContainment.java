7d3161b541295d818cdfb750006e905ba4ec5009
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-01-09 18:31:17 -0500

    TriangleContainment threaded implementation
    I can't use ForkJoinPool here because we can't split an arbitrary Iterable
    in half without materializing it into a List, which wouldn't be a fair
    comparison.
    
    The threaded implementation runs in about 16s on my Surface Pro 2,
    compared to 27s for the StreamJIT implementation.  The StreamJIT
    implementation is strangely insensitive to the multiplier, so perhaps
    we're doing something wrong there.

--- src/edu/mit/streamjit/test/apps/TriangleContainment.java
+++ src/edu/mit/streamjit/test/apps/TriangleContainment.java
@@ -1,7 +1,9 @@
 package edu.mit.streamjit.test.apps;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
+import com.google.common.util.concurrent.Uninterruptibles;
 import edu.mit.streamjit.api.Filter;
 import edu.mit.streamjit.api.Input;
 import edu.mit.streamjit.api.OneToOneElement;
@@ -12,8 +14,13 @@ import edu.mit.streamjit.impl.interp.DebugStreamCompiler;
 import edu.mit.streamjit.test.AbstractBenchmark;
 import edu.mit.streamjit.test.Benchmarker;
 import java.awt.Polygon;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Parses string representations of triangles, then tests whether they contain
@@ -21,7 +28,7 @@ import java.util.Random;
  * triangle contained the origin or not.  Loosely based on Project Euler problem
  * 102, "Triangle Containment".
  *
- * This file also contains an implementation using ForkJoinPool, for
+ * This file also contains an implementation using threads, for
  * comparison purposes.  It simply computes a count of origin-containing
  * triangles, rather than a list of booleans.
  * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
@@ -38,7 +45,7 @@ public final class TriangleContainment {
 		@Override
 		public void work() {
 			for (String s : pop().split(","))
-				push(Integer.valueOf(s));
+				push(Integer.parseInt(s));
 		}
 	}
 
@@ -82,9 +89,62 @@ public final class TriangleContainment {
 		return Iterables.concat(Collections.nCopies(REPETITIONS, list.build()));
 	}
 
+	private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+	private static int runThreads() {
+		Iterator<String> taskIterator = generateInput().iterator();
+		AtomicInteger result = new AtomicInteger(0);
+		List<Thread> threads = new ArrayList<>(NUM_THREADS);
+		for (int i = 0; i < NUM_THREADS; ++i)
+			threads.add(new ComputeThread(taskIterator, result));
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		for (Thread t : threads)
+			t.start();
+		for (Thread t : threads)
+			Uninterruptibles.joinUninterruptibly(t);
+		System.out.println("Thread impl ran in " + stopwatch.stop().elapsed(TimeUnit.MILLISECONDS));
+		return result.get();
+	}
+
+	private static final class ComputeThread extends Thread {
+		private static final int STRINGS_PER_TASK = 100000;
+		private static final Object TASK_ITERATOR_LOCK = new Object();
+		private final Iterator<String> taskIterator;
+		private final AtomicInteger result;
+		private ComputeThread(Iterator<String> taskIterator, AtomicInteger result) {
+			this.taskIterator = taskIterator;
+			this.result = result;
+		}
+
+		@Override
+		public void run() {
+			List<String> tasks = new ArrayList<>(STRINGS_PER_TASK);
+			while (true) {
+				tasks.clear();
+				synchronized (TASK_ITERATOR_LOCK) {
+					for (int i = 0; i < STRINGS_PER_TASK && taskIterator.hasNext(); ++i)
+						tasks.add(taskIterator.next());
+				}
+				if (tasks.isEmpty())
+					return;
+
+				int accum = 0;
+				for (String task : tasks) {
+					String[] data = task.split(",");
+					Polygon p = new Polygon();
+					for (int i = 0; i < TRIANGLE_SIDES*2; i += 2)
+						p.addPoint(Integer.parseInt(data[i]), Integer.parseInt(data[i+1]));
+					if (p.contains(0, 0))
+						++accum;
+				}
+				result.addAndGet(accum);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 //		StreamCompiler sc = new DebugStreamCompiler();
 		StreamCompiler sc = new Compiler2StreamCompiler().maxNumCores(4).multiplier(Short.MAX_VALUE);
 		Benchmarker.runBenchmark(new TriangleContainmentBenchmark(), sc).get(0).print(System.out);
+		runThreads();
 	}
 }
