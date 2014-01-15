02dcd390cc24e442df4a5af40887eac1ddc960d7
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-01-15 01:12:41 -0500

    AlwaysStrategy, NeverStrategy
    We could use dynamic proxies instead, which would gain a little bit of
    type safety because the proxy would only implement one of the strategy
    interfaces.

---
+++ src/edu/mit/streamjit/impl/compiler2/AlwaysStrategy.java
@@ -0,0 +1,42 @@
+package edu.mit.streamjit.impl.compiler2;
+
+import edu.mit.streamjit.api.Worker;
+import edu.mit.streamjit.impl.common.Configuration;
+import java.util.Set;
+
+/**
+ * A strategy that doesn't use parameters and always does whatever it does.
+ * @author Jeffrey Bosboom <jeffreybosboom@gmail.com>
+ * @since 1/15/2014
+ */
+public final class AlwaysStrategy implements RemovalStrategy, FusionStrategy, UnboxingStrategy {
+	@Override
+	public void makeParameters(Set<Worker<?, ?>> workers, Configuration.Builder builder) {
+		//no parameters necessary
+	}
+
+	@Override
+	public boolean remove(WorkerActor a, Configuration config) {
+		return true;
+	}
+
+	@Override
+	public boolean fuseUpward(ActorGroup group, Configuration config) {
+		return true;
+	}
+
+	@Override
+	public boolean unboxStorage(Storage storage, Configuration config) {
+		return true;
+	}
+
+	@Override
+	public boolean unboxInput(WorkerActor actor, Configuration config) {
+		return true;
+	}
+
+	@Override
+	public boolean unboxOutput(WorkerActor actor, Configuration config) {
+		return true;
+	}
+}
