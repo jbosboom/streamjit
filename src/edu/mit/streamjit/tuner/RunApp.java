570d9c0459dc9810ada86e21e4f076aaf929ae95
sumanan <sumanan@csail.mit.edu>
2014-01-30 01:48:53 +0800

    Code cleanup
    Obselete methods have been removed. These method became obselete after the
    last commit.

--- src/edu/mit/streamjit/tuner/RunApp.java
+++ src/edu/mit/streamjit/tuner/RunApp.java
@@ -1,26 +1,18 @@
 package edu.mit.streamjit.tuner;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.google.common.base.Splitter;
-
 import edu.mit.streamjit.api.StreamCompiler;
 import edu.mit.streamjit.impl.common.Configuration;
 import edu.mit.streamjit.impl.common.Configuration.IntParameter;
-import edu.mit.streamjit.impl.common.Configuration.Parameter;
-import edu.mit.streamjit.impl.common.Configuration.SwitchParameter;
 import edu.mit.streamjit.impl.compiler.CompilerStreamCompiler;
-import edu.mit.streamjit.impl.compiler2.Compiler2StreamCompiler;
 import edu.mit.streamjit.impl.distributed.DistributedStreamCompiler;
 import edu.mit.streamjit.test.Benchmark;
 import edu.mit.streamjit.test.Benchmarker;
-import edu.mit.streamjit.test.Datasets;
 import edu.mit.streamjit.tuner.ConfigGenerator.sqliteAdapter;
 
 /**
@@ -116,64 +108,5 @@ public class RunApp {
 		String qry = String.format(
 				"UPDATE results SET Exectime=%f WHERE Round=%d", time, round);
 		sjDb.executeUpdate(qry);
-	}
-
-	private static String getConfigurationString(String s) {
-		String s1 = s.replaceAll("__class__", "ttttt");
-		String s2 = s1.replaceAll("javaClassPath", "class");
-		String s3 = s2.replaceAll("ttttt", "__class__");
-		return s3;
-	}
-
-	/**
-	 * Creates a new {@link Configuration} from the received python dictionary
-	 * string. This is not a good way to do.
-	 * <p>
-	 * TODO: Need to add a method to {@link Configuration} so that the
-	 * configuration object can be updated from the python dict string. Now we
-	 * are destructing the old confg object and recreating a new one every time.
-	 * Not a appreciatable way.
-	 * 
-	 * @param pythonDict
-	 *            Python dictionary string. Autotuner gives a dictionary of
-	 *            features with trial values.
-	 * @param config
-	 *            Old configuration object.
-	 * @return New configuration object with updated values from the pythonDict.
-	 */
-	private static Configuration rebuildConfiguration(String pythonDict,
-			Configuration config) {
-		// System.out.println(pythonDict);
-		checkNotNull(pythonDict, "Received Python dictionary is null");
-		pythonDict = pythonDict.replaceAll("u'", "");
-		pythonDict = pythonDict.replaceAll("':", "");
-		pythonDict = pythonDict.replaceAll("\\{", "");
-		pythonDict = pythonDict.replaceAll("\\}", "");
-		Splitter dictSplitter = Splitter.on(", ").omitEmptyStrings()
-				.trimResults();
-		Configuration.Builder builder = Configuration.builder();
-		System.out.println("New parameter values from Opentuner...");
-		for (String s : dictSplitter.split(pythonDict)) {
-			String[] str = s.split(" ");
-			if (str.length != 2)
-				throw new AssertionError("Wrong python dictionary...");
-			Parameter p = config.getParameter(str[0]);
-			if (p == null)
-				continue;
-			if (p instanceof IntParameter) {
-				IntParameter ip = (IntParameter) p;
-				builder.addParameter(new IntParameter(ip.getName(),
-						ip.getMin(), ip.getMax(), Integer.parseInt(str[1])));
-
-			} else if (p instanceof SwitchParameter<?>) {
-				SwitchParameter sp = (SwitchParameter) p;
-				Class<?> type = sp.getGenericParameter();
-				int val = Integer.parseInt(str[1]);
-				SwitchParameter<?> sp1 = new SwitchParameter(sp.getName(),
-						type, sp.getUniverse().get(val), sp.getUniverse());
-				builder.addParameter(sp1);
-			}
-		}
-		return builder.build();
 	}
 }
