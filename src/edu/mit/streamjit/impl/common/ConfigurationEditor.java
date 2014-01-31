11be67acddffa510f0f6be80dc99c0dd6660aef8
sumanan <sumanan@csail.mit.edu>
2014-01-31 17:01:06 +0800

    Converts pythonDict to Configuration JSON

--- src/edu/mit/streamjit/impl/common/ConfigurationEditor.java
+++ src/edu/mit/streamjit/impl/common/ConfigurationEditor.java
@@ -1,30 +1,41 @@
 package edu.mit.streamjit.impl.common;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
 
+import com.google.common.base.Splitter;
+
 import edu.mit.streamjit.api.OneToOneElement;
 import edu.mit.streamjit.api.Worker;
 import edu.mit.streamjit.impl.blob.BlobFactory;
+import edu.mit.streamjit.impl.common.Configuration.IntParameter;
 import edu.mit.streamjit.impl.common.Configuration.Parameter;
 import edu.mit.streamjit.impl.common.Configuration.SwitchParameter;
 import edu.mit.streamjit.impl.distributed.DistributedBlobFactory;
+import edu.mit.streamjit.test.apps.channelvocoder7.ChannelVocoder7;
+import edu.mit.streamjit.test.apps.fmradio.FMRadio;
+import edu.mit.streamjit.test.sanity.nestedsplitjoinexample.NestedSplitJoin;
+import edu.mit.streamjit.util.json.Jsonifiers;
 
 public class ConfigurationEditor {
 
 	static String name;
 	static int noofwrks;
+
 	/**
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		edit(name, noofwrks);
+		 generate(new ChannelVocoder7.ChannelVocoder7Kernel());
+		 edit(name, noofwrks);
 		// print("4366NestedSplitJoinCore.cfg");
+		// convert();
 	}
 
 	private static void generate(OneToOneElement<?, ?> stream) {
@@ -108,5 +119,128 @@ public class ConfigurationEditor {
 				System.out.println(sp.getName() + " - " + sp.getValue());
 			}
 		}
+	}
+
+	private static void convert() {
+		String appName = "ChannelVocoder7Kernel";
+		Configuration cfg = readConfiguration(String.format("%d%s", 1, appName));
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(
+					String.format("%d%s.cfg", 0, appName)));
+			String pythonDict = reader.readLine();
+
+			Configuration finalCfg = rebuildConfiguration(pythonDict, cfg);
+			saveConfg(finalCfg, 0);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * Creates a new {@link Configuration} from the received python dictionary
+	 * string. This is not a good way to do.
+	 * <p>
+	 * TODO: Need to add a method to {@link Configuration} so that the
+	 * configuration object can be updated from the python dict string. Now we
+	 * are destructing the old confg object and recreating a new one every time.
+	 * Not a appreciatable way.
+	 * 
+	 * @param pythonDict
+	 *            Python dictionary string. Autotuner gives a dictionary of
+	 *            features with trial values.
+	 * @param config
+	 *            Old configuration object.
+	 * @return New configuration object with updated values from the pythonDict.
+	 */
+	private static Configuration rebuildConfiguration(String pythonDict,
+			Configuration config) {
+		// System.out.println(pythonDict);
+		pythonDict = pythonDict.replaceAll("u'", "");
+		pythonDict = pythonDict.replaceAll("':", "");
+		pythonDict = pythonDict.replaceAll("\\{", "");
+		pythonDict = pythonDict.replaceAll("\\}", "");
+		Splitter dictSplitter = Splitter.on(", ").omitEmptyStrings()
+				.trimResults();
+		Configuration.Builder builder = Configuration.builder();
+		System.out.println("New parameter values from Opentuner...");
+		for (String s : dictSplitter.split(pythonDict)) {
+			String[] str = s.split(" ");
+			if (str.length != 2)
+				throw new AssertionError("Wrong python dictionary...");
+			Parameter p = config.getParameter(str[0]);
+			if (p == null)
+				continue;
+			// System.out.println(String.format("\t%s = %s", str[0], str[1]));
+			if (p instanceof IntParameter) {
+				IntParameter ip = (IntParameter) p;
+				builder.addParameter(new IntParameter(ip.getName(),
+						ip.getMin(), ip.getMax(), Integer.parseInt(str[1])));
+
+			} else if (p instanceof SwitchParameter<?>) {
+				SwitchParameter sp = (SwitchParameter) p;
+				Class<?> type = sp.getGenericParameter();
+				int val = Integer.parseInt(str[1]);
+				SwitchParameter<?> sp1 = new SwitchParameter(sp.getName(),
+						type, sp.getUniverse().get(val), sp.getUniverse());
+				builder.addParameter(sp1);
+			}
+
+		}
+		return builder.build();
+	}
+
+	/**
+	 * TODO: This method is totally unnecessary if we remove the usage of the
+	 * name "class" in side {@link Configuration}.
+	 * 
+	 * @param cfg
+	 * @return
+	 */
+	private static String getConfigurationString(Configuration cfg) {
+		String s = Jsonifiers.toJson(cfg).toString();
+		String s1 = s.replaceAll("__class__", "ttttt");
+		String s2 = s1.replaceAll("class", "javaClassPath");
+		String s3 = s2.replaceAll("ttttt", "__class__");
+		return s3;
+	}
+
+	/**
+	 * Save the configuration.
+	 */
+	private static void saveConfg(Configuration config, int round) {
+		String json = config.toJson();
+		String name = "erer";
+		try {
+
+			File dir = new File(String.format("configurations%s%s",
+					File.separator, name));
+			if (!dir.exists())
+				if (!dir.mkdirs()) {
+					System.err.println("Make directory failed");
+					return;
+				}
+
+			File file = new File(dir, String.format("%d%s.cfg", round, name));
+			FileWriter writer = new FileWriter(file, false);
+			writer.write(json);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Configuration readConfiguration(String simpeName) {
+		String name = String.format("%s.cfg", simpeName);
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(name));
+			String json = reader.readLine();
+			reader.close();
+			return Configuration.fromJson(json);
+		} catch (Exception ex) {
+			System.err.println(String.format(
+					"File reader error. No %s configuration file.", name));
+		}
+		return null;
 	}
 }
