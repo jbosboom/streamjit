e8a7475de90b8b51d565c667edc21efe609c9c03
sumanan <sumanan@csail.mit.edu>
2014-03-11 12:08:37 +0800

    Binds a DynamicBufferManager to a blob.
    An instance of DynamicBufferManger can generate dynamic buffer of a single
    blob only. Same instance cannot be used to generate dynamic buffers for several
    blobs.

--- src/edu/mit/streamjit/impl/distributed/node/DynamicBufferManager.java
+++ src/edu/mit/streamjit/impl/distributed/node/DynamicBufferManager.java
@@ -4,19 +4,25 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import edu.mit.streamjit.impl.blob.Blob;
 import edu.mit.streamjit.impl.blob.Buffer;
+import edu.mit.streamjit.impl.blob.Blob.Token;
 import edu.mit.streamjit.util.ConstructorSupplier;
 import edu.mit.streamjit.util.ReflectionUtils;
 
 /**
- * Provides dynamic buffer backed by a buffer implementation which is passes an
- * argument to {@link #getBuffer(Class, List, int, int)} method. <b> Use a
+ * Provides dynamic buffer backed by a buffer implementation which is passed as
+ * an argument to {@link #getBuffer(Class, List, int, int)} method. <b> Use a
  * {@link DynamicBufferManager} per blob ( i.e., one to one mapping between a
  * blob and a DynamicBufferManager). Do not use same instance of
- * {@link DynamicBufferManager} to create buffers for multiple blobs. </b>
+ * {@link DynamicBufferManager} to create buffers for multiple blobs or multiple
+ * {@link DynamicBufferManager}s to make buffer for a single blob. Doing this
+ * will affect the deadlock detection algorithm and ultimately results either
+ * infinite buffer growth or unsolvable deadlock. </b>
  * 
  * @author sumanan
  * @since Mar 10, 2014
@@ -25,13 +31,25 @@ import edu.mit.streamjit.util.ReflectionUtils;
 public final class DynamicBufferManager {
 
 	/**
+	 * inputs of the blob in which this object is bound to.
+	 */
+	private final Set<Token> inputs;
+
+	/**
+	 * outputs of the blob in which this object is bound to.
+	 */
+	private final Set<Token> outputs;
+
+	/**
 	 * keeps track of all buffers created for a particular blob. We need to
 	 * track this list to determine whether there is an actual deadlock or this
 	 * blob is faster than all down blobs.
 	 */
 	private List<Buffer> buffers;
 
-	public DynamicBufferManager() {
+	public DynamicBufferManager(Blob blob) {
+		inputs = blob.getInputs();
+		outputs = blob.getOutputs();
 		buffers = new ArrayList<>();
 	}
 
@@ -53,14 +71,20 @@ public final class DynamicBufferManager {
 	 *            : the position of size parameter in the bufferClass.
 	 * @return : A dynamic buffer backed by an instance of bufferClass.
 	 */
-	public Buffer getBuffer(String name, Class<? extends Buffer> bufferClass,
+	public Buffer getBuffer(Token t, Class<? extends Buffer> bufferClass,
 			List<?> initialArguments, int initialCapacity, int capacityPos) {
-		Buffer buf = new DynamicBuffer(name, bufferClass, initialArguments,
-				initialCapacity, capacityPos);
+
+		if (!(outputs.contains(t) || inputs.contains(t)))
+			throw new IllegalAccessError(
+					String.format(
+							"%s is not related to the blob in which this dynamic buffer manager is bound to",
+							t.toString()));;
+
+		Buffer buf = new DynamicBuffer(t.toString(), bufferClass,
+				initialArguments, initialCapacity, capacityPos);
 		buffers.add(buf);
 		return buf;
 	}
-
 	/**
 	 * Dynamically increases supplied buffer capacity in order to avoid dead
 	 * locks. Actually creates a new instance of the supplied buffer and copy
