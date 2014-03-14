dff4a8aafc62292cf8db42c5b15eab10d30746cc
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-03-14 17:38:02 -0400

    Copy-paste bug in exception detail string

--- src/edu/mit/streamjit/impl/compiler2/InternalArrayConcreteStorage.java
+++ src/edu/mit/streamjit/impl/compiler2/InternalArrayConcreteStorage.java
@@ -50,7 +50,7 @@ public class InternalArrayConcreteStorage implements ConcreteStorage {
 		try {
 			return readHandle.invoke(index);
 		} catch (Throwable ex) {
-			throw new AssertionError(String.format("%s.read(%d, %s)", this, index), ex);
+			throw new AssertionError(String.format("%s.read(%d)", this, index), ex);
 		}
 	}
 
