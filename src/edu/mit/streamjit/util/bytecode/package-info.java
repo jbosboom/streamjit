f75cadf4fc9965025e0b486e24fc1cdd4f8bca47
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-10-16 11:05:18 -0400

    Remove util.bytecode
    We're now using bytecodelib.

--- src/edu/mit/streamjit/util/bytecode/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the bytecode transformation framework.
- */
-package edu.mit.streamjit.util.bytecode;
--- src/edu/mit/streamjit/util/bytecode/types/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Defines type classes used in the bytecode manipulation IR.
- */
-package edu.mit.streamjit.util.bytecode.types;
