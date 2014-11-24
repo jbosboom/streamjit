7e195416f82d384909568b51c143a0f1f4d66374
sumanan <sumanan@csail.mit.edu>
2014-11-24 17:37:56 +0800

    Eclipse's sort members : TimeLoggers.java

--- src/edu/mit/streamjit/impl/distributed/TimeLoggers.java
+++ src/edu/mit/streamjit/impl/distributed/TimeLoggers.java
@@ -16,29 +16,32 @@ import edu.mit.streamjit.impl.common.TimeLogger;
  */
 public class TimeLoggers {
 
+	public static class FileTimeLogger extends TimeLoggerImpl {
+
+		private static FileWriter getFileWriter(String name) {
+			FileWriter fw = null;
+			try {
+				fw = new FileWriter(name);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return fw;
+		}
+
+		FileTimeLogger() {
+			super(getFileWriter("compileTime.txt"),
+					getFileWriter("runTime.txt"),
+					getFileWriter("drainTime.txt"));
+		}
+	}
+
 	/**
 	 * Logs nothing.
 	 */
 	public static class NoTimeLogger implements TimeLogger {
 
 		@Override
-		public void newReconfiguration() {
-		}
-
-		@Override
 		public void logCompileTime(long time) {
-		}
-
-		@Override
-		public void logRunTime(long time) {
-		}
-
-		@Override
-		public void logDrainTime(long time) {
-		}
-
-		@Override
-		public void logDrainDataCollectionTime(long time) {
 		}
 
 		@Override
@@ -46,103 +49,27 @@ public class TimeLoggers {
 		}
 
 		@Override
-		public void logDrainTime(String msg) {
-		}
-
-		@Override
-		public void logRunTime(String msg) {
-		}
-
-	}
-
-	private static class TimeLoggerImpl implements TimeLogger {
-
-		private final OutputStreamWriter compileTimeWriter;
-
-		private final OutputStreamWriter runTimeWriter;
-
-		private final OutputStreamWriter drainTimeWriter;
-
-		private int reconfigNo = 0;
-
-		TimeLoggerImpl(OutputStreamWriter compileW, OutputStreamWriter runW,
-				OutputStreamWriter drainW) {
-			compileTimeWriter = compileW;
-			runTimeWriter = runW;
-			drainTimeWriter = drainW;
-		}
-
-		TimeLoggerImpl(OutputStream compileOS, OutputStream runOs,
-				OutputStream drainOs) {
-			compileTimeWriter = getOSWriter(compileOS);
-			runTimeWriter = getOSWriter(runOs);
-			drainTimeWriter = getOSWriter(drainOs);
-		}
-
-		private OutputStreamWriter getOSWriter(OutputStream os) {
-			if (os == null)
-				return null;
-			return new OutputStreamWriter(os);
-		}
-
-		@Override
-		public void newReconfiguration() {
-			reconfigNo++;
-			String msg = String
-					.format("----------------------------%d----------------------------",
-							reconfigNo);
-			write(compileTimeWriter, msg);
-			write(runTimeWriter, msg);
-			write(drainTimeWriter, msg);
-		}
-
-		private void write(OutputStreamWriter osWriter, String msg) {
-			if (osWriter != null) {
-				try {
-					osWriter.write(msg);
-					osWriter.flush();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		@Override
-		public void logCompileTime(long time) {
-			write(compileTimeWriter,
-					String.format("Total compile time %dms\n", time));
-		}
-
-		@Override
-		public void logRunTime(long time) {
-			write(runTimeWriter,
-					String.format("Execution time is %dms\n", time));
+		public void logDrainDataCollectionTime(long time) {
 		}
 
 		@Override
 		public void logDrainTime(long time) {
-			write(drainTimeWriter, String.format("Drain time is %dms\n", time));
-		}
-
-		@Override
-		public void logDrainDataCollectionTime(long time) {
-			write(drainTimeWriter,
-					String.format("Drain data collection time is %dms\n", time));
-		}
-
-		@Override
-		public void logCompileTime(String msg) {
-			write(compileTimeWriter, msg);
 		}
 
 		@Override
 		public void logDrainTime(String msg) {
-			write(drainTimeWriter, msg);
+		}
+
+		@Override
+		public void logRunTime(long time) {
 		}
 
 		@Override
 		public void logRunTime(String msg) {
-			write(runTimeWriter, msg);
+		}
+
+		@Override
+		public void newReconfiguration() {
 		}
 
 	}
@@ -158,22 +85,95 @@ public class TimeLoggers {
 		}
 	}
 
-	public static class FileTimeLogger extends TimeLoggerImpl {
+	private static class TimeLoggerImpl implements TimeLogger {
 
-		FileTimeLogger() {
-			super(getFileWriter("compileTime.txt"),
-					getFileWriter("runTime.txt"),
-					getFileWriter("drainTime.txt"));
+		private final OutputStreamWriter compileTimeWriter;
+
+		private final OutputStreamWriter drainTimeWriter;
+
+		private int reconfigNo = 0;
+
+		private final OutputStreamWriter runTimeWriter;
+
+		TimeLoggerImpl(OutputStream compileOS, OutputStream runOs,
+				OutputStream drainOs) {
+			compileTimeWriter = getOSWriter(compileOS);
+			runTimeWriter = getOSWriter(runOs);
+			drainTimeWriter = getOSWriter(drainOs);
 		}
 
-		private static FileWriter getFileWriter(String name) {
-			FileWriter fw = null;
-			try {
-				fw = new FileWriter(name);
-			} catch (IOException e) {
-				e.printStackTrace();
+		TimeLoggerImpl(OutputStreamWriter compileW, OutputStreamWriter runW,
+				OutputStreamWriter drainW) {
+			compileTimeWriter = compileW;
+			runTimeWriter = runW;
+			drainTimeWriter = drainW;
+		}
+
+		@Override
+		public void logCompileTime(long time) {
+			write(compileTimeWriter,
+					String.format("Total compile time %dms\n", time));
+		}
+
+		@Override
+		public void logCompileTime(String msg) {
+			write(compileTimeWriter, msg);
+		}
+
+		@Override
+		public void logDrainDataCollectionTime(long time) {
+			write(drainTimeWriter,
+					String.format("Drain data collection time is %dms\n", time));
+		}
+
+		@Override
+		public void logDrainTime(long time) {
+			write(drainTimeWriter, String.format("Drain time is %dms\n", time));
+		}
+
+		@Override
+		public void logDrainTime(String msg) {
+			write(drainTimeWriter, msg);
+		}
+
+		@Override
+		public void logRunTime(long time) {
+			write(runTimeWriter,
+					String.format("Execution time is %dms\n", time));
+		}
+
+		@Override
+		public void logRunTime(String msg) {
+			write(runTimeWriter, msg);
+		}
+
+		@Override
+		public void newReconfiguration() {
+			reconfigNo++;
+			String msg = String
+					.format("----------------------------%d----------------------------",
+							reconfigNo);
+			write(compileTimeWriter, msg);
+			write(runTimeWriter, msg);
+			write(drainTimeWriter, msg);
+		}
+
+		private OutputStreamWriter getOSWriter(OutputStream os) {
+			if (os == null)
+				return null;
+			return new OutputStreamWriter(os);
+		}
+
+		private void write(OutputStreamWriter osWriter, String msg) {
+			if (osWriter != null) {
+				try {
+					osWriter.write(msg);
+					osWriter.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-			return fw;
 		}
+
 	}
 }
