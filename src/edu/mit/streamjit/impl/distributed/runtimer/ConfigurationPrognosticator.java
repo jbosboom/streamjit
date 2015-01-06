f5b280927821c3a3801d4b1d3234bbb3c9b9abf0
sumanan <sumanan@csail.mit.edu>
2015-01-06 21:20:01 +0800

    ConfigurationPrognosticator added
    An interface ConfigurationPrognosticator and a concrete class NoPrognostication
    have been added. ConfigurationPrognosticator prognosticates the configurations
    given by the OpenTuner and tell whether a configuration is more likely to give
    a better search objective improvement or not.

---
+++ src/edu/mit/streamjit/impl/distributed/runtimer/ConfigurationPrognosticator.java
@@ -0,0 +1,42 @@
+package edu.mit.streamjit.impl.distributed.runtimer;
+
+import edu.mit.streamjit.impl.common.Configuration;
+
+/**
+ * Prognosticates the {@link Configuration}s given by the OpenTuner and tell
+ * whether a {@link Configuration} is more likely to give a better search
+ * objective improvement or not. Depends on the prognosticated information,
+ * {@link OnlineTuner} may reconfigure the application or reject the
+ * configuration. Currently, the search objective is performance optimization.
+ * In future, some other resource optimization objectives may be added (e.g.,
+ * Energy minimization).
+ * 
+ * @author sumanan
+ * @since 6 Jan, 2015
+ */
+public interface ConfigurationPrognosticator {
+
+	/**
+	 * Prognosticate a {@link Configuration} and tell whether a
+	 * {@link Configuration} is more likely to give a better search objective
+	 * improvement or not.
+	 * 
+	 * @param config
+	 * @return {@code true} iff the config is more likely to give a better
+	 *         search objective improvement.
+	 */
+	public boolean prognosticate(Configuration config);
+
+	/**
+	 * No Prognostication. The method {@link #prognosticate(Configuration)}
+	 * always returns {@code true}
+	 */
+	public static final class NoPrognostication implements
+			ConfigurationPrognosticator {
+
+		@Override
+		public boolean prognosticate(Configuration config) {
+			return true;
+		}
+	}
+}
