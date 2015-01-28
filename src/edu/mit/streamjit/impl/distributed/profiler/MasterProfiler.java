bef71c937a431bb8a5d3aa7b942ec9cc571c90f4
sumanan <sumanan@csail.mit.edu>
2015-01-28 23:53:09 +0800

    MasterProfiler.print(SNBufferStatusData) added.

--- src/edu/mit/streamjit/impl/distributed/profiler/MasterProfiler.java
+++ src/edu/mit/streamjit/impl/distributed/profiler/MasterProfiler.java
@@ -1,10 +1,11 @@
 package edu.mit.streamjit.impl.distributed.profiler;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-import edu.mit.streamjit.impl.blob.Blob.Token;
 import edu.mit.streamjit.impl.distributed.profiler.SNProfileElement.SNBufferStatusData;
+import edu.mit.streamjit.impl.distributed.profiler.SNProfileElement.SNBufferStatusData.BlobBufferStatus;
+import edu.mit.streamjit.impl.distributed.profiler.SNProfileElement.SNBufferStatusData.BufferStatus;
 import edu.mit.streamjit.impl.distributed.profiler.SNProfileElement.SNProfileElementProcessor;
 
 /**
@@ -15,14 +16,31 @@ import edu.mit.streamjit.impl.distributed.profiler.SNProfileElement.SNProfileEle
  */
 public class MasterProfiler implements SNProfileElementProcessor {
 
-	private final Map<Token, SNBufferStatusData> BufferStatusDataMap;
+	private final Map<Integer, SNBufferStatusData> BufferStatusDataMap;
+
+	private Object lock = new Object();
 
 	public MasterProfiler() {
-		BufferStatusDataMap = new HashMap<>();
+		BufferStatusDataMap = new ConcurrentHashMap<>();
 	}
 
 	@Override
 	public void process(SNBufferStatusData bufferStatusData) {
-		BufferStatusDataMap.put(bufferStatusData.blobID, bufferStatusData);
+		BufferStatusDataMap.put(bufferStatusData.machineID, bufferStatusData);
+		print(bufferStatusData);
+	}
+
+	private void print(SNBufferStatusData bufferStatusData) {
+		synchronized (lock) {
+			System.out.println("MachineID=" + bufferStatusData.machineID);
+			for (BlobBufferStatus bbs : bufferStatusData.blobsBufferStatusSet) {
+				System.out.println("Blob - " + bbs.blobID);
+				System.out.println("Input buffers...");
+				for (BufferStatus bs : bbs.inputSet)
+					System.out.println(bs);
+				for (BufferStatus bs : bbs.outputSet)
+					System.out.println(bs);
+			}
+		}
 	}
 }
