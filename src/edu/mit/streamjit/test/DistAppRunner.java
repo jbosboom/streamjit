3c782d793b42415aef6e12057441ad8a0b8b04ae
sumanan <sumanan@csail.mit.edu>
2015-02-25 21:49:51 +0800

    DistAppRunner updates total running time to README

--- src/edu/mit/streamjit/test/DistAppRunner.java
+++ src/edu/mit/streamjit/test/DistAppRunner.java
@@ -3,6 +3,9 @@ package edu.mit.streamjit.test;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
 
 import edu.mit.streamjit.api.CompiledStream;
 import edu.mit.streamjit.api.Input;
@@ -22,7 +25,7 @@ public final class DistAppRunner {
 	public static void main(String[] args) throws InterruptedException,
 			IOException {
 		int noOfNodes;
-
+		Stopwatch sw = Stopwatch.createStarted();
 		try {
 			noOfNodes = Integer.parseInt(args[0]);
 		} catch (Exception ex) {
@@ -41,8 +44,10 @@ public final class DistAppRunner {
 				Output.blackHole());
 		stream.awaitDrained();
 
+		sw.stop();
+		long elapsedMills = sw.elapsed(TimeUnit.MILLISECONDS);
 		String appName = streamGraph.getClass().getSimpleName();
-		updateReadMeTxt(appName, benchmark.toString());
+		updateReadMeTxt(appName, benchmark.toString(), elapsedMills);
 	}
 
 	private static void startSNs(int noOfNodes) throws IOException {
@@ -58,11 +63,23 @@ public final class DistAppRunner {
 	 * Consider passing the benchmarkName to the
 	 * {@link DistributedStreamCompiler} and let it to update the README.txt.
 	 */
-	public static void updateReadMeTxt(String appName, String benchmarkName)
-			throws IOException {
+	private static void updateReadMeTxt(String appName, String benchmarkName,
+			long elapsedMills) throws IOException {
 		FileWriter writer = new FileWriter(String.format("%s%sREADME.txt",
 				appName, File.separator), true);
 		writer.write("benchmarkName=" + benchmarkName);
+		writer.write("TotalRunningTime=" + elapsedTime(elapsedMills));
 		writer.close();
+	}
+
+	private static String elapsedTime(long mills) {
+		String hms = String.format(
+				"%02dH:%02dM:%02dS",
+				TimeUnit.MILLISECONDS.toHours(mills),
+				TimeUnit.MILLISECONDS.toMinutes(mills)
+						% TimeUnit.HOURS.toMinutes(1),
+				TimeUnit.MILLISECONDS.toSeconds(mills)
+						% TimeUnit.MINUTES.toSeconds(1));
+		return hms;
 	}
 }
