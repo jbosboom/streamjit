862f71b0809d50c5ba0c7a3196c736a48a75751a
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2015-03-08 13:25:54 -0400

    TuneExternalStorageStrategy: lambda

--- src/edu/mit/streamjit/impl/compiler2/TuneExternalStorageStrategy.java
+++ src/edu/mit/streamjit/impl/compiler2/TuneExternalStorageStrategy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 Massachusetts Institute of Technology
+ * Copyright (c) 2013-2015 Massachusetts Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -47,22 +47,19 @@ public final class TuneExternalStorageStrategy implements StorageStrategy {
 	}
 	@Override
 	public StorageFactory asFactory(final Configuration config) {
-		return new StorageFactory() {
-			@Override
-			public ConcreteStorage make(Storage storage) {
-				if (storage.steadyStateCapacity() == 0)
-					return new EmptyConcreteStorage(storage);
+		return (Storage storage) -> {
+			if (storage.steadyStateCapacity() == 0)
+				return new EmptyConcreteStorage(storage);
 
-				Configuration.SwitchParameter<Arrayish.Factory> factoryParam = config.getParameter("ExternalArrayish"+storage.id(), Configuration.SwitchParameter.class, Arrayish.Factory.class);
-				Arrayish.Factory factory = storage.type().isPrimitive() ? factoryParam.getValue() : Arrayish.ArrayArrayish.factory();
-				Configuration.SwitchParameter<Boolean> useDoubleBuffersParam = config.getParameter("UseDoubleBuffers"+storage.id(), Configuration.SwitchParameter.class, Boolean.class);
-				if (useDoubleBuffersParam.getValue()
-						&& storage.steadyStateCapacity() == 2*storage.throughput() //no leftover data
-						&& storage.isFullyExternal() //no reads of writes before adjust
-						)
-					return new DoubleArrayConcreteStorage(factory, storage);
-				return new CircularArrayConcreteStorage(factory.make(storage.type(), storage.steadyStateCapacity()), storage);
-			}
+			Configuration.SwitchParameter<Arrayish.Factory> factoryParam = config.getParameter("ExternalArrayish"+storage.id(), Configuration.SwitchParameter.class, Arrayish.Factory.class);
+			Arrayish.Factory factory = storage.type().isPrimitive() ? factoryParam.getValue() : Arrayish.ArrayArrayish.factory();
+			Configuration.SwitchParameter<Boolean> useDoubleBuffersParam = config.getParameter("UseDoubleBuffers"+storage.id(), Configuration.SwitchParameter.class, Boolean.class);
+			if (useDoubleBuffersParam.getValue()
+					&& storage.steadyStateCapacity() == 2*storage.throughput() //no leftover data
+					&& storage.isFullyExternal() //no reads of writes before adjust
+					)
+				return new DoubleArrayConcreteStorage(factory, storage);
+			return new CircularArrayConcreteStorage(factory.make(storage.type(), storage.steadyStateCapacity()), storage);
 		};
 	}
 }
